c0b0df65a8f71bc8c40bebe5eb2ebecd54a7a40d
amazingalek <alek.ntnu@gmail.com>
2019-12-20 06:11:36 +0100

    Version check (#10)
    notifying user when mod is made for wrong version of OWML

--- OWML.Common/ModManifest.cs
+++ OWML.Common/ModManifest.cs
@@ -19,6 +19,9 @@ namespace OWML.Common
         [JsonProperty("version")]
         public string Version { get; private set; }
 
+        [JsonProperty("owmlVersion")]
+        public string OWMLVersion { get; private set; }
+
         [JsonProperty("enabled")]
         public bool Enabled { get; private set; }
 
