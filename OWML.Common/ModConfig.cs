b5b3580f0b9066b4a7036f972cd6fe50d549df89
Aleksander Waage <alek.ntnu@gmail.com>
2019-12-20 17:24:52 +0100

    is verbose is true in config: logging all errors globally

--- OWML.Common/ModConfig.cs
+++ OWML.Common/ModConfig.cs
@@ -6,7 +6,10 @@ namespace OWML.Common
     {
         [JsonProperty("gamePath")]
         public string GamePath { get; set; }
-        
+
+        [JsonProperty("verbose")]
+        public bool Verbose { get; private set; }
+
         [JsonIgnore]
         public string ManagedPath => $"{GamePath}/OuterWilds_Data/Managed";
 
@@ -15,7 +18,7 @@ namespace OWML.Common
 
         [JsonIgnore]
         public string LogFilePath => $"{OWMLPath}Logs/OWML.Log.txt";
-
+        
         [JsonIgnore]
         public string OutputFilePath => $"{OWMLPath}Logs/OWML.Output.txt";
 
--- OWML.Common/ModHelper.cs
+++ OWML.Common/ModHelper.cs
@@ -3,13 +3,15 @@ namespace OWML.Common
 {
     public class ModHelper : IModHelper
     {
+        public IModConfig Config { get; }
         public IModLogger Logger { get; }
         public IModConsole Console { get; }
         public IModEvents Events { get; }
         public IHarmonyHelper HarmonyHelper { get; }
 
-        public ModHelper(IModLogger logger, IModConsole console, IModEvents events, IHarmonyHelper harmonyHelper)
+        public ModHelper(IModConfig config, IModLogger logger, IModConsole console, IModEvents events, IHarmonyHelper harmonyHelper)
         {
+            Config = config;
             Logger = logger;
             Console = console;
             Events = events;
