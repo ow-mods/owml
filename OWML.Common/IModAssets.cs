f1d061113a6888d79fc77acce5f8577281366b9e
Aleksander Waage <alek.ntnu@gmail.com>
2019-12-23 21:24:27 +0100

    Assets.LoadMesh and Assets.LoadTexture

--- OWML.Common/IModAssets.cs
+++ OWML.Common/IModAssets.cs
@@ -5,6 +5,8 @@ namespace OWML.Common
     public interface IModAssets
     {
         GameObject Load3DObject(ModBehaviour modBehaviour, string objectFilename, string imageFilename);
+        MeshFilter LoadMesh(ModBehaviour modBehaviour, string objectFilename);
+        MeshRenderer LoadTexture(ModBehaviour modBehaviour, string imageFilename);
         AudioSource LoadAudio(ModBehaviour modBehaviour, string audioFilename);
     }
 }
