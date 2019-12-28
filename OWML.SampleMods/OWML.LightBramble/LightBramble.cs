f57a10e8a911f36c7652823b435dd7a636b1469a
amazingalek <alek.ntnu@gmail.com>
2019-12-29 00:49:32 +0100

    Light bramble 2 (#34)
    * "TestMod" is replaced by new sample mod: Light Bramble. No fog. No Anglerfish. Nice music.
    * Bugfixes related to modhelper being shared by multiple mods

---
+++ OWML.SampleMods/OWML.LightBramble/LightBramble.cs
@@ -0,0 +1,90 @@
+﻿using OWML.Common;
+using OWML.ModHelper.Events;
+using OWML.ModHelper;
+using UnityEngine;
+
+namespace OWML.LightBramble
+{
+    public class LightBramble : ModBehaviour
+    {
+        private AudioSource _dekuSource;
+        private bool _hasBrambleLoaded;
+
+        private void Start()
+        {
+            ModHelper.Console.WriteLine("In " + nameof(LightBramble));
+
+            ModHelper.Events.AddEvent<AnglerfishController>(Events.AfterEnable);
+            ModHelper.Events.AddEvent<FogWarpVolume>(Events.AfterAwake);
+            ModHelper.Events.AddEvent<PlanetaryFogController>(Events.AfterEnable);
+            ModHelper.Events.AddEvent<FogOverrideVolume>(Events.AfterAwake);
+            ModHelper.Events.AddEvent<GlobalMusicController>(Events.AfterStart);
+            ModHelper.Events.OnEvent += OnEvent;
+
+            var audioAsset = ModHelper.Assets.LoadAudio("deku-tree.mp3");
+            audioAsset.OnLoaded += OnLoaded;
+        }
+
+        private void OnLoaded(AudioSource dekuSource)
+        {
+            ModHelper.Console.WriteLine("Deku Tree music loaded!");
+            if (dekuSource.clip == null)
+            {
+                ModHelper.Console.WriteLine("Clip is null :(");
+            }
+            _dekuSource = dekuSource;
+        }
+
+        private void OnEvent(MonoBehaviour behaviour, Events ev)
+        {
+            if (behaviour.GetType() == typeof(AnglerfishController) && ev == Events.AfterEnable)
+            {
+                ModHelper.Console.WriteLine("Deactivating anglerfish");
+                behaviour.gameObject.SetActive(false);
+            }
+            else if (behaviour.GetType().IsSubclassOf(typeof(FogWarpVolume)) && ev == Events.AfterAwake)
+            {
+                ModHelper.Console.WriteLine("Clearing _fogColor in FogWarpVolume");
+                behaviour.SetValue("_fogColor", Color.clear);
+            }
+            else if (behaviour.GetType() == typeof(PlanetaryFogController) && ev == Events.AfterEnable)
+            {
+                ModHelper.Console.WriteLine("Clearing _fogTint in PlanetaryFogController");
+                behaviour.SetValue("_fogTint", Color.clear);
+            }
+            else if (behaviour.GetType() == typeof(FogOverrideVolume) && ev == Events.AfterAwake)
+            {
+                ModHelper.Console.WriteLine("Clearing _tint in FogOverrideVolume");
+                behaviour.SetValue("_tint", Color.clear);
+            }
+            else if (behaviour.GetType() == typeof(GlobalMusicController) && ev == Events.AfterStart)
+            {
+                ModHelper.Console.WriteLine("Swapping _darkBrambleSource in GlobalMusicController");
+                behaviour.SetValue("_darkBrambleSource", null);
+                _hasBrambleLoaded = true;
+            }
+            else
+            {
+                ModHelper.Console.WriteLine("Missing check: " + behaviour.GetType().Name);
+            }
+        }
+
+        private void Update()
+        {
+            if (_hasBrambleLoaded)
+            {
+                var shouldPlay = Locator.GetPlayerSectorDetector().InBrambleDimension() && !Locator.GetPlayerSectorDetector().InVesselDimension() && PlayerState.AtFlightConsole() && !PlayerState.IsHullBreached();
+                if (shouldPlay && !_dekuSource.isPlaying)
+                {
+                    _dekuSource.Play();
+                }
+                else if (!shouldPlay && _dekuSource.isPlaying)
+                {
+                    _dekuSource.Stop();
+                }
+            }
+        }
+
+    }
+
+}
