b277e4f14a31fefbf9af9dc9aed6248d2e300ac8
Aleksander Waage <alek.ntnu@gmail.com>
2020-01-10 22:28:26 +0100

    wip

--- OWML.ModHelper.Menus/ModMenus.cs
+++ OWML.ModHelper.Menus/ModMenus.cs
@@ -1,4 +1,5 @@
 ﻿using OWML.Common;
+using UnityEngine;
 
 namespace OWML.ModHelper.Menus
 {
@@ -7,15 +8,21 @@ namespace OWML.ModHelper.Menus
         public IModMenu MainMenu { get; }
         public IModPopupMenu PauseMenu { get; }
 
+        private readonly IModConsole _console;
+
         public ModMenus(IModLogger logger, IModConsole console)
         {
+            _console = console;
             MainMenu = new ModMainMenu(logger, console);
             PauseMenu = new ModPauseMenu(logger, console);
         }
 
         public IModPopupMenu CreateMenu()
         {
-            throw new System.NotImplementedException();
+            var original = PauseMenu ?? MainMenu;
+
+            var copy = GameObject.Instantiate(original);
+
         }
 
     }
