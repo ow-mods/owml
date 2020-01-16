a3f4c17cec769a1294690ab2705e7934ba6eb22f
Aleksander Waage <alek.ntnu@gmail.com>
2020-01-16 18:25:47 +0100

    wip

---
+++ OWML.ModHelper.Menus/ModPauseMenu.cs
@@ -0,0 +1,15 @@
+﻿using OWML.Common;
+
+namespace OWML.ModHelper.Menus
+{
+    public class ModPauseMenu : ModPopupMenu
+    {
+        private IModButton Resume;
+        private IModButton Options;
+        private IModButton Quit;
+        
+        public ModPauseMenu(IModLogger logger, IModConsole console) : base(logger, console)
+        {
+        }
+    }
+}
