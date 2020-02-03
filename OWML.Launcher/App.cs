db2ecdfaf67997839c7c22b50548ec5b90743062
Aleksander Waage <alek.ntnu@gmail.com>
2020-02-03 21:12:47 +0100

    0.3.32

--- OWML.Launcher/App.cs
+++ OWML.Launcher/App.cs
@@ -12,7 +12,7 @@ namespace OWML.Launcher
 {
     public class App
     {
-        private const string Version = "0.3.31";
+        private const string Version = "0.3.32";
 
         private readonly IOwmlConfig _owmlConfig;
         private readonly IModConsole _writer;
