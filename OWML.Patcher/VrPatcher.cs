ba022480632bc0c733848107ef43ace1ab0c383c
Aleksander Waage <alek.ntnu@gmail.com>
2020-02-05 21:28:43 +0100

    rewrite

--- OWML.Patcher/VrPatcher.cs
+++ OWML.Patcher/VrPatcher.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
@@ -10,12 +9,6 @@ namespace OWML.Patcher
 {
     public class VRPatcher
     {
-        private static readonly List<string> PatchChecksums = new List<string>
-        {
-            "d3979abb3b0d2468c3e03e2ee862d5297f5885bd9fc8f3b16cb16805e010d097",
-            "todo"
-        };
-
         private readonly IOwmlConfig _owmlConfig;
         private readonly IModConsole _writer;
         private readonly SHA256 _sha;
@@ -54,34 +47,29 @@ namespace OWML.Patcher
         private void PatchGlobalManager(bool enableVR)
         {
             var originalPath = _owmlConfig.DataPath + "/globalgamemanagers";
-            var backupPath = _owmlConfig.DataPath + "/globalgamemanagers.bak";
-            var vrPath = _owmlConfig.DataPath + "/globalgamemanagers.vr";
-
             if (!File.Exists(originalPath))
             {
                 _writer.WriteLine("Error: can't find " + originalPath);
                 return;
             }
 
-            var gameVersionHasChanged = !OriginalIsSameSizeAsBackupOrVrVersion(originalPath, backupPath, vrPath);
-            if (gameVersionHasChanged)
-            {
-                _writer.WriteLine("Looks like new game version!");
-            }
+            var checksum = CalculateChecksum(originalPath);
+            var backupPath = $"{_owmlConfig.DataPath}/globalgamemanagers.{checksum}.bak";
+            var vrPath = $"{_owmlConfig.DataPath}/globalgamemanagers.{checksum}.vr";
 
-            if (!File.Exists(backupPath) || gameVersionHasChanged)
+
+            if (!File.Exists(backupPath))
             {
                 _writer.WriteLine("Taking backup of globalgamemanagers.");
                 File.Copy(originalPath, backupPath, true);
             }
 
-            if (enableVR && (!File.Exists(vrPath) || gameVersionHasChanged))
+            if (enableVR && !File.Exists(vrPath))
             {
                 _writer.WriteLine("Patching globalgamemanagers for VR...");
-                var checksum = CalculateChecksum(originalPath);
-                if (PatchChecksums.Contains(checksum))
+                var patchPath = $"{_owmlConfig.OWMLPath}VR/{checksum}";
+                if (File.Exists(patchPath))
                 {
-                    var patchPath = $"{_owmlConfig.OWMLPath}VR/{checksum}";
                     ApplyPatch(originalPath, vrPath, patchPath);
                 }
                 else
@@ -93,14 +81,6 @@ namespace OWML.Patcher
 
             var copyFrom = enableVR ? vrPath : backupPath;
             File.Copy(copyFrom, originalPath, true);
-        }
-
-        private bool OriginalIsSameSizeAsBackupOrVrVersion(string originalPath, string backupPath, string vrPath)
-        {
-            var originalSize = File.ReadAllBytes(originalPath).Length;
-            var backupSize = File.ReadAllBytes(backupPath).Length;
-            var vrSize = File.Exists(vrPath) ? File.ReadAllBytes(vrPath).Length : 0;
-            return originalSize == backupSize || originalSize == vrSize;
         }
 
         private string CalculateChecksum(string filePath)
