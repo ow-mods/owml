99ac1a50400915b8c58b2446aa391761ade160a5
Ricardo Lopes <raicuparta@gmail.com>
2020-02-20 08:56:42 +0100

    Allow specifying parameters for method patching

--- OWML.Common/IHarmonyHelper.cs
+++ OWML.Common/IHarmonyHelper.cs
@@ -4,9 +4,9 @@ namespace OWML.Common
 {
     public interface IHarmonyHelper
     {
-        void AddPrefix<T>(string methodName, Type patchType, string patchMethodName);
-        void AddPostfix<T>(string methodName, Type patchType, string patchMethodName);
-        void EmptyMethod<T>(string methodName);
-        void Transpile<T>(string methodName, Type patchType, string patchMethodName);
+        void AddPrefix<T>(string methodName, Type patchType, string patchMethodName, Type[] parameters = null);
+        void AddPostfix<T>(string methodName, Type patchType, string patchMethodName, Type[] parameters = null);
+        void EmptyMethod<T>(string methodName, Type[] parameters = null);
+        void Transpile<T>(string methodName, Type patchType, string patchMethodName, Type[] parameters = null);
     }
 }
