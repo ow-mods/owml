641303b3a896e6adbe7208e87a1b1913fababb5a
AmazingAlek <alek.ntnu@gmail.com>
2020-03-03 21:16:05 +0100

    0.3.39 (#81)

--- OWML.Launcher/App.cs
+++ OWML.Launcher/App.cs
@@ -12,7 +12,7 @@ namespace OWML.Launcher
 {
     public class App
     {
-        private const string Version = "0.3.38";
+        private const string Version = "0.3.39";
 
         private readonly IOwmlConfig _owmlConfig;
         private readonly IModConsole _writer;
