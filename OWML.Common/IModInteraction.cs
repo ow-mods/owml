c02369f57b08ba316c9f6837c7e9a48131c1cf24
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2020-04-26 20:18:51 +0100

    Added more helper methods

--- OWML.Common/IModInteraction.cs
+++ OWML.Common/IModInteraction.cs
@@ -8,8 +8,11 @@ namespace OWML.Common
     public interface IModInteraction
     {
         IList<IModData> GetMods();
+        IList<IModData> GetDependants(string dependencyName);
         bool ModExists(string uniqueName);
         T InvokeMethod<T>(string uniqueName, string methodName, params object[] parameters);
+        void InvokeMethod(string uniqueName, string methodName, params object[] parameters);
         void SetVariableValue(string uniqueName, string variableName, object value);
+        T GetVariableValue<T>(string uniqueName, string variableName);
     }
 }
--- OWML.ModHelper.Interaction/ModInteraction.cs
+++ OWML.ModHelper.Interaction/ModInteraction.cs
@@ -9,15 +9,38 @@ namespace OWML.ModHelper.Interaction
     {
         private readonly List<ModBehaviour> _modList = new List<ModBehaviour>();
         private readonly IModFinder _finder;
+        private readonly Dictionary<IModData, List<IModData>> dependantDict = new Dictionary<IModData, List<IModData>>();
+
         public ModInteraction(List<ModBehaviour> list, IModFinder finder)
         {
             _modList = list;
             _finder = finder;
+            // Creates dictionary of key [Dependency] and value [Mods that depend on that dependency]
+            foreach (var mod in _finder.GetMods())
+            {
+                List<IModData> temp = new List<IModData>();
+                foreach (var mod2 in _finder.GetMods())
+                {
+                    if (mod2.Manifest.Dependencies != null)
+                    {
+                        if (mod2.Manifest.Dependencies.Contains(mod.Manifest.Name))
+                        {
+                            temp.Add(mod2);
+                        }
+                    }
+                }
+                dependantDict.Add(mod, temp);
+            }
         }
 
         public IList<IModData> GetMods()
         {
             return _finder.GetMods();
+        }
+
+        public IList<IModData> GetDependants(string dependencyName)
+        {
+            return dependantDict.Where(x => x.Key.Manifest.UniqueName == dependencyName).FirstOrDefault().Value;
         }
 
         public bool ModExists(string uniqueName)
@@ -31,10 +54,22 @@ namespace OWML.ModHelper.Interaction
             return (T)mod.GetType().GetMethod(methodName).Invoke(mod, parameters);
         }
 
+        public void InvokeMethod(string uniqueName, string methodName, params object[] parameters)
+        {
+            var mod = _modList.Where(m => m.ModHelper.Manifest.UniqueName == uniqueName).First();
+            mod.GetType().GetMethod(methodName).Invoke(mod, parameters);
+        }
+
         public void SetVariableValue(string uniqueName, string variableName, object value)
         {
             var mod = _modList.Where(m => m.ModHelper.Manifest.UniqueName == uniqueName).First();
             mod.GetType().SetValue(variableName, value);
+        }
+
+        public T GetVariableValue<T>(string uniqueName, string variableName)
+        {
+            var mod = _modList.Where(m => m.ModHelper.Manifest.UniqueName == uniqueName).First();
+            return mod.GetType().GetValue<T>(variableName);
         }
     }
 }
