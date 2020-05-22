bdd848e1d52fb42d85e7eb90da4489bc8e0793f8
Aleksander Waage <alek.ntnu@gmail.com>
2020-05-22 22:12:17 +0200

    fixed controller input bug introduced in PR #99

--- OWML.ModHelper/ControllerButton.cs
+++ OWML.ModHelper/ControllerButton.cs
@@ -1,12 +1,18 @@
 ﻿using OWML.ModHelper.Events;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace OWML.ModHelper
 {
     public class ControllerButton : MonoBehaviour
     {
+        private Button _button;
+        private SingleAxisCommand _inputCommand;
+
         public void Init(SingleAxisCommand inputCommand)
         {
+            _button = GetComponent<Button>();
+            _inputCommand = inputCommand;
             var imageElement = GetComponent<ButtonWithHotkeyImageElement>();
             if (imageElement == null)
             {
@@ -15,6 +21,14 @@ namespace OWML.ModHelper
             var textId = GetComponentInChildren<LocalizedText>().GetValue<UITextType>("_textID");
             var title = UITextLibrary.GetString(textId);
             imageElement.SetPrompt(new ScreenPrompt(inputCommand, title));
+        }
+
+        private void Update()
+        {
+            if (_inputCommand != null && _button != null && OWInput.IsPressed(_inputCommand, InputMode.Menu))
+            {
+                _button.onClick.Invoke();
+            }
         }
     }
 }
