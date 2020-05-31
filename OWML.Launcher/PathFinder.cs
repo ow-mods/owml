9f0f79029553f807072e6be76608bf30fee64245
Aleksander Waage <alek.ntnu@gmail.com>
2020-05-31 13:52:29 +0200

    cleaned up path finder and added steam support. WIP.

--- OWML.Launcher/PathFinder.cs
+++ OWML.Launcher/PathFinder.cs
@@ -20,57 +20,59 @@ namespace OWML.Launcher
 
         public string FindGamePath()
         {
+            return ValidateCurrentPath() ??
+                   FindGameInDefaultFolders() ??
+                   FindGameInEGS() ??
+                   FindGameInSteam() ??
+                   PromptGamePath();
+        }
+
+        private string ValidateCurrentPath()
+        {
             if (IsValidGamePath(_config.GamePath))
             {
                 return _config.GamePath;
             }
-            _writer.WriteLine("Game path is not correct.");
-
-            var gamePath = FindInDefaultFolders();
-            if (!string.IsNullOrEmpty(gamePath))
-            {
-                return gamePath;
-            }
-            _writer.WriteLine("Game not found in default folders.");
-
-            gamePath = FindInRegistry();
-            if (!string.IsNullOrEmpty(gamePath))
-            {
-                return gamePath;
-            }
-            _writer.WriteLine("Game not found in registry.");
-
-            return PromptGamePath();
+            _writer.WriteLine($"Current game path is not valid: {_config.GamePath}");
+            return null;
         }
 
-        private string FindInDefaultFolders()
+        private string FindGameInDefaultFolders()
         {
             var paths = new[]
             {
-                AppDomain.CurrentDomain.BaseDirectory + "..",
-                "C:/Program Files/Epic Games/OuterWilds",
-                "D:/Program Files/Epic Games/OuterWilds",
-                "E:/Program Files/Epic Games/OuterWilds",
-                "F:/Program Files/Epic Games/OuterWilds",
+                $"{AppDomain.CurrentDomain.BaseDirectory}..",
                 "C:/Program Files (x86)/Outer Wilds",
                 "D:/Program Files (x86)/Outer Wilds",
                 "E:/Program Files (x86)/Outer Wilds",
-                "F:/Program Files (x86)/Outer Wilds"
+                "F:/Program Files (x86)/Outer Wilds",
+                "C:/Games/Outer Wilds",
+                "D:/Games/Outer Wilds",
+                "E:/Games/Outer Wilds",
+                "F:/Games/Outer Wilds"
             };
-            return paths.FirstOrDefault(IsValidGamePath);
+            var gamePath = paths.FirstOrDefault(IsValidGamePath);
+            if (!string.IsNullOrEmpty(gamePath))
+            {
+                return gamePath;
+            }
+            _writer.WriteLine("Game not found in default locations.");
+            return null;
         }
 
-        private string FindInRegistry()
+        private string FindGameInEGS()
         {
             var key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\Epic Games\EpicGamesLauncher");
             var appDataPath = (string)key?.GetValue("AppDataPath");
             if (string.IsNullOrEmpty(appDataPath))
             {
+                _writer.WriteLine("EGS not found in Registry.");
                 return null;
             }
-            var manifestsPath = appDataPath + "Manifests";
+            var manifestsPath = $"{appDataPath}Manifests";
             if (!Directory.Exists(manifestsPath))
             {
+                _writer.WriteLine($"EGS manifests folder not found: {manifestsPath}");
                 return null;
             }
             var manifestPaths = Directory.GetFiles(manifestsPath, "*.item", SearchOption.TopDirectoryOnly);
@@ -83,6 +85,51 @@ namespace OWML.Launcher
                     return epicManifest.InstallLocation;
                 }
             }
+            _writer.WriteLine("Game not found in EGS.");
+            return null;
+        }
+
+        private string FindGameInSteam()
+        {
+            var key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Valve\Steam");
+            var steamPath = (string)key?.GetValue("SteamPath");
+            if (string.IsNullOrEmpty(steamPath))
+            {
+                _writer.WriteLine("Steam not found in Registry.");
+                return null;
+            }
+            var defaultLocation = $"{steamPath}/steamapps/common/Outer Wilds";
+            if (IsValidGamePath(defaultLocation))
+            {
+                return defaultLocation;
+            }
+            var libraryFoldersFile = $"{steamPath}/steamapps/libraryfolders.vdf";
+            if (!File.Exists(libraryFoldersFile))
+            {
+                _writer.WriteLine($"Steam library folders file not found: {libraryFoldersFile}");
+                return null;
+            }
+            var libraryFoldersContent = File.ReadAllText(libraryFoldersFile);
+            for (var i = 1; i < 10; i++)
+            {
+                var libraryStart = $"\"{i}\"\t\t\"";
+                var startIndex = libraryFoldersContent.IndexOf(libraryStart);
+                if (startIndex < 0)
+                {
+                    _writer.WriteLine("Game not found in custom Steam library.");
+                    return null;
+                }
+                var afterStartIndex = startIndex + libraryStart.Length;
+                var endIndex = libraryFoldersContent.IndexOf('\"', afterStartIndex);
+                var length = endIndex - afterStartIndex;
+                var libraryPath = libraryFoldersContent.Substring(afterStartIndex, length);
+                var gamePath = $"{libraryPath}/steamapps/common/Outer Wilds";
+                if (IsValidGamePath(gamePath))
+                {
+                    return gamePath;
+                }
+            }
+            _writer.WriteLine("Game not found in Steam.");
             return null;
         }
 
@@ -91,11 +138,10 @@ namespace OWML.Launcher
             var gamePath = _config.GamePath;
             while (!IsValidGamePath(gamePath))
             {
-                _writer.WriteLine("Game not found at " + gamePath);
+                _writer.WriteLine($"Game not found at {gamePath}");
                 _writer.WriteLine("Please enter the correct game path:");
                 gamePath = Console.ReadLine()?.Trim();
             }
-            _writer.WriteLine($"Game found at {_config.GamePath}");
             return gamePath;
         }
 
