89e4fb4e317d5bd8253265c11d58c3f6502e7b8a
Aleksandr <TAImatem@gmail.com>
2020-07-11 13:58:40 +0500

    implemented modPromptButton (#172)
    * Implemented ModPromptButton

---
+++ OWML.ModHelper.Menus/ModPromptButton.cs
@@ -0,0 +1,35 @@
+﻿using OWML.Common.Menus;
+using OWML.ModHelper.Events;
+using UnityEngine.UI;
+
+namespace OWML.ModHelper.Menus
+{
+    public class ModPromptButton : ModButton, IModPromptButton
+    {
+        private ScreenPrompt _prompt;
+        private UITextType _textID;
+        private readonly ButtonWithHotkeyImageElement _hotkeyButton;
+
+        public string DefaultTitle => UITextLibrary.GetString(_textID);
+        public ScreenPrompt Prompt
+        {
+            get => _prompt;
+            set
+            {
+                _prompt = value;
+                _hotkeyButton.SetPrompt(value);
+            }
+        }
+
+        public ModPromptButton(Button button, IModMenu menu) : base(button, menu)
+        {
+            _hotkeyButton = Button.GetComponent<ButtonWithHotkeyImageElement>();
+            if (_hotkeyButton == null)
+            {
+                ModConsole.Instance.WriteLine("Error: can't setup ModPromptButton for this button");
+                return;
+            }
+            _textID = Button.GetComponentInChildren<LocalizedText>(true)?.GetValue<UITextType>("_textID") ?? UITextType.None;
+        }
+    }
+}
