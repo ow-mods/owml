745e3c55b79eabb7534687f4d82f359e7795ac51
Aleksandr <TAImatem@gmail.com>
2020-07-11 13:41:06 +0500

    removed Copy-es improved initialization

--- OWML.Common/Menus/IModPromptButton.cs
+++ OWML.Common/Menus/IModPromptButton.cs
@@ -4,15 +4,5 @@
     {
         string DefaultTitle { get; }
         ScreenPrompt Prompt { get; set; }
-
-        new IModPromptButton Copy();
-        IModPromptButton Copy(string title);
-        IModPromptButton Copy(string title, int index);
-
-        IModPromptButton Duplicate(string title);
-        IModPromptButton Duplicate(string title, int index);
-
-        IModPromptButton Replace(string title);
-        IModPromptButton Replace(string title, int index);
     }
 }
--- OWML.ModHelper.Menus/ModPromptButton.cs
+++ OWML.ModHelper.Menus/ModPromptButton.cs
@@ -1,6 +1,5 @@
 ﻿using OWML.Common.Menus;
 using OWML.ModHelper.Events;
-using UnityEngine;
 using UnityEngine.UI;
 
 namespace OWML.ModHelper.Menus
@@ -28,62 +27,12 @@ namespace OWML.ModHelper.Menus
         public ModPromptButton(Button button, IModMenu menu) : base(button, menu)
         {
             _hotkeyButton = Button.GetComponent<ButtonWithHotkeyImageElement>();
-            _textID = Button.GetComponentInChildren<LocalizedText>(true).GetValue<UITextType>("_textID");
             if (_hotkeyButton == null)
             {
                 ModConsole.Instance.WriteLine("Error: can't setup ModPromptButton for this button");
                 return;
             }
+            _textID = Button.GetComponentInChildren<LocalizedText>(true)?.GetValue<UITextType>("_textID") ?? UITextType.None;
         }
-
-        public new IModPromptButton Copy()
-        {
-            var button = (IModPromptButton)base.Copy();
-            button.Prompt = Prompt;
-            return button;
-        }
-
-        public IModPromptButton Copy(string title)
-        {
-            var copy = Copy();
-            copy.Prompt = Prompt;
-            return copy;
-        }
-
-        public IModPromptButton Copy(string title, int index)
-        {
-            var copy = Copy(title);
-            copy.Index = index;
-            return copy;
-        }
-
-        public IModPromptButton Duplicate(string title)
-        {
-            var dupe = (IModPromptButton)Duplicate();
-            dupe.Prompt = Prompt;
-            return dupe;
-        }
-
-        public IModPromptButton Duplicate(string title, int index)
-        {
-            var dupe = Duplicate(title);
-            dupe.Index = index;
-            return dupe;
-        }
-
-        public IModPromptButton Replace(string title)
-        {
-            var replacement = (IModPromptButton)Replace();
-            replacement.Prompt = Prompt;
-            return replacement;
-        }
-
-        public IModPromptButton Replace(string title, int index)
-        {
-            var replacement = Replace(title);
-            replacement.Index = index;
-            return replacement;
-        }
-
     }
 }
