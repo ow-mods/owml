50e30cc109d775b3a2ca5d03eb6550b335ddda76
Aleksander Waage <alek.ntnu@gmail.com>
2020-07-15 01:09:22 +0200

    * removed gamepath from OWML config menu * updated config section in readme

--- OWML.ModHelper.Menus/OwmlConfigMenu.cs
+++ OWML.ModHelper.Menus/OwmlConfigMenu.cs
@@ -4,9 +4,8 @@ namespace OWML.ModHelper.Menus
 {
     public class OwmlConfigMenu : ModConfigMenuBase
     {
-        private const string GamePathTitle = "Game path";
         private const string VerboseModeTitle = "Verbose mode";
-        private const string BlockInputTitle = "Mod button combinations block game input";
+        private const string BlockInputTitle = "Mod input combinations block game input";
 
         private readonly IOwmlConfig _config;
         private readonly IOwmlConfig _defaultConfig;
@@ -21,7 +20,6 @@ namespace OWML.ModHelper.Menus
         protected override void AddInputs()
         {
             var index = 2;
-            AddConfigInput(GamePathTitle, _config.GamePath, index++);
             AddConfigInput(VerboseModeTitle, _config.Verbose, index++);
             AddConfigInput(BlockInputTitle, _config.BlockInput, index++);
             UpdateNavigation();
@@ -30,14 +28,12 @@ namespace OWML.ModHelper.Menus
 
         protected override void UpdateUIValues()
         {
-            GetTextInput(GamePathTitle).Value = _config.GamePath;
             GetToggleInput(VerboseModeTitle).Value = _config.Verbose;
             GetToggleInput(BlockInputTitle).Value = _config.BlockInput;
         }
 
         protected override void OnSave()
         {
-            _config.GamePath = (string)GetInputValue(GamePathTitle);
             _config.Verbose = (bool)GetInputValue(VerboseModeTitle);
             _config.BlockInput = (bool)GetInputValue(BlockInputTitle);
             Storage.Save(_config, Constants.OwmlConfigFileName);
@@ -46,7 +42,6 @@ namespace OWML.ModHelper.Menus
 
         protected override void OnReset()
         {
-            _config.GamePath = _defaultConfig.GamePath;
             _config.Verbose = _defaultConfig.Verbose;
             _config.BlockInput = _defaultConfig.BlockInput;
             UpdateUIValues();
