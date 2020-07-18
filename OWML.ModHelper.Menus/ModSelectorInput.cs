2a1e91d80b9a20584e6a9108b84587f92ec97dc8
Aleksandr <TAImatem@gmail.com>
2020-07-19 00:14:27 +0500

    overflow protection

--- OWML.ModHelper.Menus/ModSelectorInput.cs
+++ OWML.ModHelper.Menus/ModSelectorInput.cs
@@ -9,22 +9,26 @@ namespace OWML.ModHelper.Menus
         public override bool IsSelected => _element?.GetValue<bool>("_amISelected") ?? false;
 
         private readonly OptionsSelectorElement _element;
+        private int _count;
 
         public ModSelectorInput(OptionsSelectorElement element, IModMenu menu) : base(element, menu)
         {
             _element = element;
+            _count = element.GetValue<string[]>("_optionsList").Length;
             element.OnValueChanged += InvokeOnChange;
         }
 
         public void Initialize(int index, string[] options)
         {
+            _count = options.Length;
+            index %= _count;
             _element.Initialize(index, options);
         }
 
         public override int Value
         {
             get => _element.GetCurrentIndex();
-            set => _element.Initialize(value);
+            set => _element.Initialize(value % _count);
         }
 
         public IModSelectorInput Copy()
