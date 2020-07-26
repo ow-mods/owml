71e031b942a9a5bd1189482069801680235c58a1
Aleksandr <TAImatem@gmail.com>
2020-07-26 15:11:29 +0500

    Made some fixes to Listener (#240)
    * allowed Listener to handle multiple messages
    made it handle errors more gracefully
    
    * made buffer big enough to handle even stack overflow trace

--- OWML.Launcher/SocketListener.cs
+++ OWML.Launcher/SocketListener.cs
@@ -11,7 +11,8 @@ namespace OWML.Launcher
 {
     public class SocketListener
     {
-        private const int BufferSize = 1024;
+        private const string Separator = "\n--------------------------------";
+        private const int BufferSize = 262144;
         private static int _port;
         private static TcpListener _server;
         private static IOwmlConfig _config;
@@ -43,6 +44,10 @@ namespace OWML.Launcher
             {
                 ConsoleUtils.WriteByType(MessageType.Error, $"Error in socket listener: {ex}");
             }
+            catch (Exception ex)
+            {
+                ConsoleUtils.WriteByType(MessageType.Error, $"Error while listening: {ex}");
+            }
             finally
             {
                 _server?.Stop();
@@ -73,23 +78,40 @@ namespace OWML.Launcher
                     ProcessMessage(bytes, i);   
                 }
 
+                ConsoleUtils.WriteByType(MessageType.Success, "Closing client!");
                 client.Close();
             }
         }
 
         private void ProcessMessage(byte[] bytes, int count)
         {
-            var json = Encoding.UTF8.GetString(bytes, 0, count);
-
-            var data = JsonConvert.DeserializeObject<SocketMessage>(json);
-
-            if (data.Type == MessageType.Quit)
+            var message = Encoding.UTF8.GetString(bytes, 0, count);
+            var jsons = message.Split('\n');
+            foreach (var json in jsons)
             {
-                Environment.Exit(0);
-            }
+                if (string.IsNullOrWhiteSpace(json))
+                {
+                    continue;
+                }
+                SocketMessage data;
+                try
+                {
+                    data = JsonConvert.DeserializeObject<SocketMessage>(json);
+                }
+                catch (JsonReaderException ex)
+                {
+                    ConsoleUtils.WriteByType(MessageType.Warning, $"Failed to process following message:{Separator}\n{json}{Separator}");
+                    ConsoleUtils.WriteByType(MessageType.Warning, $"Reason: {ex.Message}{Separator}");
+                    continue;
+                }
 
-            ConsoleUtils.WriteByType(data.Type,
-                $"[{data.SenderName}.{data.SenderType}] : {data.Message}");
+                if (data.Type == MessageType.Quit)
+                {
+                    Environment.Exit(0);
+                }
+                ConsoleUtils.WriteByType(data.Type,
+                    $"[{data.SenderName}.{data.SenderType}] : {data.Message}");
+            }
         }
     }
 }
