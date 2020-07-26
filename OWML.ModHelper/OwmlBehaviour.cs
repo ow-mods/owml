409d9583543ffa0979998e5438d07f7e0f6f0643
Aleksander Waage <alek.ntnu@gmail.com>
2020-07-26 23:33:24 +0200

    task delayer shared class

--- OWML.ModHelper/OwmlBehaviour.cs
+++ OWML.ModHelper/OwmlBehaviour.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using OWML.Common;
+﻿using OWML.Common;
 using UnityEngine;
 
 namespace OWML.ModHelper
 {
     public class OwmlBehaviour : MonoBehaviour
     {
-        private static List<Action> _actions = new List<Action>();
-
         private void Start()
         {
             DontDestroyOnLoad(gameObject);
@@ -18,17 +14,5 @@ namespace OWML.ModHelper
         {
             ModConsole.Instance.WriteLine("", MessageType.Quit);
         }
-
-        public static void FireOnNextUpdate(Action action)
-        {
-            _actions.Add(action);
-        }
-        
-        private void Update()
-        {
-            _actions.ForEach(action => action.Invoke());
-            _actions = new List<Action>();
-        }
-
     }
 }
---
+++ OWML.ModHelper/TaskDelayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OWML.ModHelper
+{
+    public class TaskDelayer : MonoBehaviour
+    {
+        private static List<Action> _actions = new List<Action>();
+
+        public static void FireOnNextUpdate(Action action)
+        {
+            _actions.Add(action);
+        }
+
+        private void Update()
+        {
+            _actions.ForEach(action => action.Invoke());
+            _actions = new List<Action>();
+        }
+    }
+}
