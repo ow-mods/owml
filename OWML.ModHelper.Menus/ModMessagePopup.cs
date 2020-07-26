fc2092104df6ea822811bf1bcb4dab6bbe47d1a7
Aleksandr <TAImatem@gmail.com>
2020-07-27 01:30:56 +0500

    Alek's suggestions

--- OWML.ModHelper.Menus/ModMessagePopup.cs
+++ OWML.ModHelper.Menus/ModMessagePopup.cs
@@ -45,15 +45,17 @@ namespace OWML.ModHelper.Menus
         {
             if (_twoButtonPopup == null || IsOpen)
             {
-                Console.WriteLine("Failed to create popup for a following message:");
-                Console.WriteLine(message);
+                OwmlConsole.WriteLine("Failed to create popup for a following message:", MessageType.Warning);
+                OwmlConsole.WriteLine(message, MessageType.Info);
             }
             IsOpen = true;
             _twoButtonPopup.OnPopupConfirm += OnPopupConfirm;
             _twoButtonPopup.OnPopupCancel += OnPopupCancel;
             _twoButtonPopup.EnableMenu(true);
+            var okPrompt = new ScreenPrompt(InputLibrary.confirm, okMessage);
+            var cancelPrompt = new ScreenPrompt(InputLibrary.cancel, cancelMessage);
             _twoButtonPopup.SetUpPopup(message, InputLibrary.confirm, addCancel ? InputLibrary.cancel : null,
-                new ScreenPrompt(InputLibrary.confirm, okMessage), new ScreenPrompt(InputLibrary.cancel, cancelMessage), true, addCancel);
+               okPrompt, okPrompt, true, addCancel);
             _twoButtonPopup.GetValue<Text>("_labelText").text = message;
         }
 
