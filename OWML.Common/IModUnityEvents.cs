331cee9b873568e6a6bf3d27331ec0e0c6e93c97
Aleksander Waage <alek.ntnu@gmail.com>
2020-07-27 11:25:54 +0200

    more unity events

--- OWML.Common/IModUnityEvents.cs
+++ OWML.Common/IModUnityEvents.cs
@@ -4,6 +4,9 @@ namespace OWML.Common
 {
     public interface IModUnityEvents
     {
+        event Action OnUpdate; 
+        event Action OnFixedUpdate; 
+        event Action OnLateUpdate; 
         void FireOnNextUpdate(Action action);
     }
 }
--- OWML.ModHelper.Events/ModUnityEvents.cs
+++ OWML.ModHelper.Events/ModUnityEvents.cs
@@ -7,6 +7,10 @@ namespace OWML.ModHelper.Events
 {
     public class ModUnityEvents : MonoBehaviour, IModUnityEvents
     {
+        public event Action OnUpdate;
+        public event Action OnFixedUpdate;
+        public event Action OnLateUpdate;
+
         private List<Action> _actions = new List<Action>();
 
         public void FireOnNextUpdate(Action action)
@@ -23,6 +27,17 @@ namespace OWML.ModHelper.Events
         {
             _actions.ForEach(action => action.Invoke());
             _actions = new List<Action>();
+            OnUpdate?.Invoke();
+        }
+
+        private void FixedUpdate()
+        {
+            OnFixedUpdate?.Invoke();
+        }
+
+        private void LateUpdate()
+        {
+            OnLateUpdate?.Invoke();
         }
 
     }
