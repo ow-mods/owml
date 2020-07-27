6a0c271630397af64cde2b491180eaab354e2e30
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2020-07-27 21:22:05 +0100

    add try catch

--- OWML.ModHelper/Logging/ModSocketOutput.cs
+++ OWML.ModHelper/Logging/ModSocketOutput.cs
@@ -90,7 +90,14 @@ namespace OWML.ModHelper
         private void WriteToSocket(string message)
         {
             var bytes = Encoding.UTF8.GetBytes(message + Environment.NewLine);
-            _socket?.Send(bytes);
+            try
+            {
+                _socket?.Send(bytes);
+            }
+            catch
+            {
+
+            }
         }
     }
 }
