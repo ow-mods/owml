e7985193a4a2f77fd51b876a2d98450127ba10fd
Aleksander Waage <alek.ntnu@gmail.com>
2020-07-27 11:27:24 +0200

    .

--- OWML.Common/IModUnityEvents.cs
+++ OWML.Common/IModUnityEvents.cs
@@ -4,9 +4,10 @@ namespace OWML.Common
 {
     public interface IModUnityEvents
     {
-        event Action OnUpdate; 
-        event Action OnFixedUpdate; 
-        event Action OnLateUpdate; 
+        event Action OnUpdate;
+        event Action OnFixedUpdate;
+        event Action OnLateUpdate;
+
         void FireOnNextUpdate(Action action);
     }
 }
