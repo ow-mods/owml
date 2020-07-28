829ab51238adc7799e2560b3ca8524295a554c5a
Aleksander Waage <alek.ntnu@gmail.com>
2020-07-28 21:11:47 +0200

    only selecting first at title screen

--- OWML.ModHelper.Menus/ModsMenu.cs
+++ OWML.ModHelper.Menus/ModsMenu.cs
@@ -151,7 +151,10 @@ namespace OWML.ModHelper.Menus
         private void OnPopupCancel()
         {
             _modConfigMenus.ForEach(modMenu => modMenu.ModData.UpdateSnapshot());
-            _menus.MainMenu.SelectFirst();
+            if (LoadManager.GetCurrentScene() == OWScene.TitleScreen)
+            {
+                _menus.MainMenu.SelectFirst();
+            }
         }
 
         private void OnPopupConfirm()
