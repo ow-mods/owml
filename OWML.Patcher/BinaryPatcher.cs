59187868c1dcbc839443385ad82d2ae315d07819
Aleksandr <TAImatem@gmail.com>
2020-07-30 13:34:13 +0500

    Generalized BinaryPatcher

--- OWML.Patcher/BinaryPatcher.cs
+++ OWML.Patcher/BinaryPatcher.cs
@@ -11,26 +11,79 @@ namespace OWML.Patcher
         private readonly IOwmlConfig _owmlConfig;
         private readonly IModConsole _writer;
 
-        // Indexes of addresses that need to be shifted due to added bytes.
-        private readonly int[] _addressIndexes = { 0x2d0, 0x2e0, 0x2f4, 0x308, 0x31c, 0x330, 0x344, 0x358, 0x36c, 0x380 };
-
         private const string EnabledVRDevice = "OpenVR";
-        private const int RemovedBytes = 2;
+        private const int VRRemovedBytes = 2;
         // String that comes right before the bytes we want to patch.
-        private const string PatchZoneText = "Assets/Scenes/PostCreditScene.unity";
-        private const int PatchStartZoneOffset = 6;
+        private const string VRPatchZoneText = "Assets/Scenes/PostCreditScene.unity";
+        private const int VRPatchStartZoneOffset = 6;
+        private const int BuildSettingsSector = 10;//count from zero
+
         private const int FileSizeStartIndex = 4;
-        private const int FileSizeEndIndex = FileSizeStartIndex + 4;
         private const string FileName = "globalgamemanagers";
         private const string BackupSuffix = ".bak";
-        private const int BuildSettingsStartAddressIndex = 0x2CC;
-        private const int BuildSettingsSizeIndex = 0x2D0;
         private const int BlockAddressOffset = 0x1000;
+        private const int FirstAddressIndex = 0x204;
+        private const int AddressStructureSize = 0x14;
+        private const int SizeOffset = 0x4;
+        private const int SectorCount = 20;
 
         public BinaryPatcher(IOwmlConfig owmlConfig, IModConsole writer)
         {
             _owmlConfig = owmlConfig;
             _writer = writer;
+        }
+
+        public byte[] GetSectorBytes(byte[] fileBytes, int sector)
+        {
+            if (sector<0 || sector>=SectorCount)
+            {
+                return new byte[0];
+            }
+
+            var sectorIndex = FirstAddressIndex + sector * AddressStructureSize;
+            var sectorStart = BitConverter.ToInt32(fileBytes, sectorIndex) + BlockAddressOffset;
+            var sectorEnd = sectorStart + BitConverter.ToInt32(fileBytes, sectorIndex + SizeOffset);
+
+            return fileBytes.Take(sectorEnd).Skip(sectorStart).ToArray();
+        }
+
+        public byte[] PatchSectionBytes(byte[] fileBytes, byte[] newBytes, int sectionStart, int sectionOriginalSize, int sector = -1)
+        {
+            if (sector == -1)
+            {
+                GetSectorIndex(fileBytes, sectionStart);
+            }
+            if (sector < 0 || sector >= SectorCount)
+            {
+                if (sector == -1)
+                {
+                    _writer.WriteLine("Error - Patching header is not supported", MessageType.Error);
+                }
+                return fileBytes;
+            }
+
+            var sectionEnd = sectionStart + sectionOriginalSize;
+
+            var sizeDifference = newBytes.Length - sectionOriginalSize;
+
+            var newFileBytes = fileBytes.Take(sectionStart).Concat(newBytes).Concat(fileBytes.Skip(sectionEnd)).ToArray();
+
+            PatchFileSize(newFileBytes, sizeDifference, sector);
+
+            return newFileBytes;
+        }
+
+        public byte[] PatchSectorBytes(byte[] fileBytes, byte[] newBytes, int sector)
+        {
+            if (sector < 0 || sector >= SectorCount)
+            {
+                return fileBytes;
+            }
+
+            var sectorIndex = FirstAddressIndex + sector * AddressStructureSize;
+            var sectorStart = BitConverter.ToInt32(fileBytes, sectorIndex) + BlockAddressOffset;
+            var sectorSize = BitConverter.ToInt32(fileBytes, sectorIndex + SizeOffset);
+            return PatchSectionBytes(fileBytes, newBytes, sectorStart, sectorSize, sector);
         }
 
         public void Patch()
@@ -43,12 +96,12 @@ namespace OWML.Patcher
 
             var fileBytes = File.ReadAllBytes(filePath);
 
-            var buildSettingsStartIndex = BitConverter.ToInt32(fileBytes, BuildSettingsStartAddressIndex) + BlockAddressOffset;
-            var buildSettingsSize = BitConverter.ToInt32(fileBytes, BuildSettingsSizeIndex);
-            var buildSettingsEndIndex = buildSettingsStartIndex + buildSettingsSize;
+            var buildSettingsSectorIndex = FirstAddressIndex + AddressStructureSize * BuildSettingsSector;
+            var buildSettingsStartIndex = BitConverter.ToInt32(fileBytes, buildSettingsSectorIndex) + BlockAddressOffset;
 
-            var patchStartIndex = FindPatchStartIndex(fileBytes, buildSettingsStartIndex, buildSettingsEndIndex);
-            var isAlreadyPatched = FindExistingPatch(fileBytes, patchStartIndex, buildSettingsEndIndex);
+            var buildSettingsBytes = GetSectorBytes(fileBytes, BuildSettingsSector);
+            var patchStartOffset = FindVRPatchStartOffset(buildSettingsBytes);
+            var isAlreadyPatched = FindExistingVRPatch(buildSettingsBytes, patchStartOffset);
 
             if (isAlreadyPatched)
             {
@@ -57,18 +110,33 @@ namespace OWML.Patcher
             }
 
             BackupFile(filePath);
-            var patchedBytes = CreatePatchedFileBytes(fileBytes, patchStartIndex);
+            var patchedBytes = CreateVRPatchFileBytes(fileBytes, buildSettingsStartIndex + patchStartOffset);
             File.WriteAllBytes(filePath, patchedBytes);
             _writer.WriteLine("Successfully patched globalgamemanagers.", MessageType.Success);
         }
 
-        private int FindPatchStartIndex(byte[] fileBytes, int startIndex, int endIndex)
+        private int GetSectorIndex(byte[] fileBytes, int address)
         {
-            var patchZoneBytes = Encoding.ASCII.GetBytes(PatchZoneText);
-            var patchZoneMatch = 0;
-            for (var i = startIndex; i < endIndex; i++)
+            int sector = -1;
+            for (int sectorIndex = 0; sectorIndex < SectorCount; sectorIndex++)
             {
-                var fileByte = fileBytes[i];
+                var sectorStart = BitConverter.ToInt32(fileBytes, FirstAddressIndex + sectorIndex * AddressStructureSize) + BlockAddressOffset;
+                if (sectorStart > address)
+                {
+                    return sector;
+                }
+                sector = sectorIndex;
+            }
+            return sector;
+        }
+
+        private int FindVRPatchStartOffset(byte[] sectorBytes)
+        {
+            var patchZoneBytes = Encoding.ASCII.GetBytes(VRPatchZoneText);
+            var patchZoneMatch = 0;
+            for (var i = 0; i < sectorBytes.Length; i++)
+            {
+                var fileByte = sectorBytes[i];
                 var patchZoneByte = patchZoneBytes[patchZoneMatch];
                 if (fileByte == patchZoneByte)
                 {
@@ -80,20 +148,20 @@ namespace OWML.Patcher
                 }
                 if (patchZoneMatch == patchZoneBytes.Length)
                 {
-                    return i + PatchStartZoneOffset;
+                    return i + VRPatchStartZoneOffset;
                 }
             }
             throw new Exception("Could not find patch zone in globalgamemanagers. This probably means the VR patch needs to be updated.");
         }
 
-        private bool FindExistingPatch(byte[] fileBytes, int startIndex, int endIndex)
+        private bool FindExistingVRPatch(byte[] sectorBytes, int startIndex)
         {
             var existingPatchBytes = Encoding.ASCII.GetBytes(EnabledVRDevice);
             var existingPatchMatch = 0;
 
-            for (var i = startIndex; i < endIndex; i++)
+            for (var i = startIndex; i < sectorBytes.Length; i++)
             {
-                var fileByte = fileBytes[i];
+                var fileByte = sectorBytes[i];
                 var existingPatchByte = existingPatchBytes[existingPatchMatch];
                 if (fileByte == existingPatchByte)
                 {
@@ -111,52 +179,46 @@ namespace OWML.Patcher
             return false;
         }
 
-        private byte[] CreatePatchedFileBytes(byte[] fileBytes, int patchStartIndex)
+        private byte[] CreateVRPatchFileBytes(byte[] fileBytes, int patchStartIndex)
         {
             // First byte is the number of elements in the array.
             var vrDevicesDeclarationBytes = new byte[] { 1, 0, 0, 0, (byte)EnabledVRDevice.Length, 0, 0, 0 };
 
             // Bytes that need to be inserted into the file.
-            var patchBytes = vrDevicesDeclarationBytes.Concat(Encoding.ASCII.GetBytes(EnabledVRDevice)).ToList();
+            var patchBytes = vrDevicesDeclarationBytes.Concat(Encoding.ASCII.GetBytes(EnabledVRDevice)).ToArray();
 
-            PatchFileSize(fileBytes, patchBytes.Count);
-
-            // Split the file in two parts. The patch bytes will be inserted between these parts.
-            var originalFirstPart = fileBytes.Take(patchStartIndex);
-            var originalSecondPart = fileBytes.Skip(patchStartIndex + RemovedBytes);
-
-            return originalFirstPart
-                .Concat(patchBytes)
-                .Concat(originalSecondPart)
-                .ToArray();
+            return PatchSectionBytes(fileBytes, patchBytes, patchStartIndex, VRRemovedBytes, BuildSettingsSector);
         }
 
-        private void PatchFileSize(byte[] fileBytes, int patchSize)
+        private void ShiftInt(byte[] fileBytes, int location, int difference, bool isBigEndian = false)
         {
-            // Read file size from original file. Reversed due to big endianness.
-            var originalFileSizeBytes = fileBytes.Take(FileSizeEndIndex).Skip(FileSizeStartIndex).Reverse().ToArray();
-            var originalFileSize = BitConverter.ToInt32(originalFileSizeBytes, 0);
+            int value = isBigEndian ?
+                BitConverter.ToInt32(fileBytes.Take(location + 4).Reverse().ToArray(), 0) :
+                BitConverter.ToInt32(fileBytes, location);
 
-            // Generate bytes for new patched file.
-            var fileSizeChange = patchSize - RemovedBytes;
-            var patchedFileSize = originalFileSize + fileSizeChange;
-            var patchedFileSizeBytes = BitConverter.GetBytes(patchedFileSize).Reverse().ToArray();
-
-            // Overwrite original file size bytes with patched size.
-            for (var i = 0; i < patchedFileSizeBytes.Length; i++)
+            var increasedValueBytes = BitConverter.GetBytes(value + difference);
+            if (isBigEndian)
             {
-                fileBytes[FileSizeStartIndex + i] = patchedFileSizeBytes[i];
+                increasedValueBytes = increasedValueBytes.Reverse().ToArray();
             }
 
-            // Shift addresses where necessary.
-            foreach (var startIndex in _addressIndexes)
+            increasedValueBytes.CopyTo(fileBytes, location);
+        }
+
+        private void PatchFileSize(byte[] fileBytes, int patchSize, int patchedSector)
+        {
+            // Shift file size.
+            ShiftInt(fileBytes, FileSizeStartIndex, patchSize, true);
+
+            // Shift sector size.
+            var sizeIndex = FirstAddressIndex + patchedSector * AddressStructureSize + SizeOffset;
+            ShiftInt(fileBytes, sizeIndex, patchSize);
+
+            // Shift conscutive sector addresses.
+            for (var sector = patchedSector + 1; sector < SectorCount; sector++)
             {
-                var address = BitConverter.ToInt32(fileBytes, startIndex);
-                var patchedAddressBytes = BitConverter.GetBytes(address + fileSizeChange);
-                for (var i = 0; i < patchedAddressBytes.Length; i++)
-                {
-                    fileBytes[startIndex + i] = patchedAddressBytes[i];
-                }
+                var startIndex = FirstAddressIndex + sector * AddressStructureSize;
+                ShiftInt(fileBytes, startIndex, patchSize);
             }
         }
 
