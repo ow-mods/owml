9e81d53069820adec3fee6d30de8520e5f549176
Aleksandr <TAImatem@gmail.com>
2020-07-30 17:50:32 +0500

    Added GameVersionReader class

---
+++ OWML.Patcher/GameVersionReader.cs
@@ -0,0 +1,19 @@
+﻿using OWML.Common;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace OWML.Patcher
+{
+    public class GameVersionReader
+    {
+        private readonly IModConsole _writer;
+        private readonly BinaryPatcher _binaryPatcher;
+
+        public GameVersionReader(IModConsole writer, BinaryPatcher binaryPatcher)
+        {
+            _writer = writer;
+            _binaryPatcher = binaryPatcher;
+        }
+    }
+}
