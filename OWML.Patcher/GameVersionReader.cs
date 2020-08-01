26600c0404f2a86f27185c53867c560b7d083987
Aleksandr <TAImatem@gmail.com>
2020-08-01 11:35:03 +0500

    3 dots minimal amount

--- OWML.Patcher/GameVersionReader.cs
+++ OWML.Patcher/GameVersionReader.cs
@@ -8,7 +8,7 @@ namespace OWML.Patcher
         private readonly BinaryPatcher _binaryPatcher;
 
         private const int PlayerSettingsSector = 0;
-        private const string VersionPattern = @"\d+(\.\d+)+";//can handle 1-infinite dots
+        private const string VersionPattern = @"\d+(\.\d+){3,}";//can handle 1-infinite dots
 
         public GameVersionReader(BinaryPatcher binaryPatcher)
         {
@@ -20,7 +20,7 @@ namespace OWML.Patcher
             var sectorBytes = _binaryPatcher.GetSectorBytes(_binaryPatcher.ReadFileBytes(), PlayerSettingsSector);
             var sectorString = Encoding.ASCII.GetString(sectorBytes);
             var match = Regex.Match(sectorString, VersionPattern);
-            return match.Success? match.Value: "not found";
+            return match.Success ? match.Value : "not found";
         }
     }
 }
