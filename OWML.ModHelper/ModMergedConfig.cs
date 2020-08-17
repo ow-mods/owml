dd7cacfe5c4437e937068b8b9760e60c36ce12ba
Ricardo Lopes <raicuparta@gmail.com>
2020-08-17 19:34:50 +0200

    Cleanup

--- OWML.ModHelper/ModMergedConfig.cs
+++ OWML.ModHelper/ModMergedConfig.cs
@@ -62,33 +62,6 @@ namespace OWML.ModHelper
             return settings;
         }
 
-        private void FixConfigs()
-        {
-            MakeConfigConsistentWithDefault();
-            SaveToStorage();
-        }
-
-        private void RemoveRedundantUserConfigEntries()
-        {
-            var toRemove = _userConfig.Settings.Keys.Except(_defaultConfig.Settings.Keys).ToList();
-            toRemove.ForEach(key => _userConfig.Settings.Remove(key));
-        }
-
-        private void MakeConfigConsistentWithDefault()
-        {
-            RemoveRedundantUserConfigEntries();
-            var keysCopy = _userConfig.Settings.Keys.ToList();
-            foreach (var key in keysCopy)
-            {
-                var userValue = _userConfig.Settings[key];
-                var defaultValue = _defaultConfig.Settings[key];
-                if (!IsSettingConsistentWithDefault(userValue, defaultValue))
-                {
-                    _userConfig.Settings.Remove(key);
-                }
-            }
-        }
-
         private object GetInnerValue(object outerValue)
         {
             if (outerValue is JObject jObject)
@@ -115,14 +88,37 @@ namespace OWML.ModHelper
             return underlyingType.IsPrimitive || underlyingType == typeof(decimal);
         }
 
-        private bool IsSettingConsistentWithDefault(object userValue, object defaultValue)
+        private bool IsSettingConsistentWithDefault(string key)
         {
+            if (!_defaultConfig.Settings.ContainsKey(key))
+            {
+                return false;
+            }
+            var userValue = _userConfig.Settings[key];
+            var defaultValue = _defaultConfig.Settings[key];
             var defaultInnerValue = GetInnerValue(defaultValue);
             if (IsNumeric(userValue) && IsNumeric(defaultInnerValue))
             {
                 return true;
             }
             return userValue.GetType() == defaultInnerValue.GetType();
+        }
+
+        private void MakeConfigConsistentWithDefault()
+        {
+            _userConfig.Settings.Keys.ToList().ForEach(key =>
+            {
+                if (!IsSettingConsistentWithDefault(key))
+                {
+                    _userConfig.Settings.Remove(key);
+                }
+            });
+        }
+
+        private void FixConfigs()
+        {
+            MakeConfigConsistentWithDefault();
+            SaveToStorage();
         }
 
         private object GetConvertedSelectorValue(string key, object value)
