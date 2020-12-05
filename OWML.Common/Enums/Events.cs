18cf1a9b9fecdfad3c04e697d0f76c0b39e1eb24
Aleksander Waage <alek.ntnu@gmail.com>
2020-12-05 09:33:15 +0100

    model newlines

--- OWML.Common/Enums/Events.cs
+++ OWML.Common/Enums/Events.cs
@@ -3,14 +3,23 @@
     public enum Events
     {
         BeforeAwake = 0,
+
         AfterAwake = 1,
+
         BeforeStart = 2,
+
         AfterStart = 3,
+
         BeforeEnable = 4,
+
         AfterEnable = 5,
+
         BeforeDisable = 6,
+
         AfterDisable = 7,
+
         BeforeDestroy = 8,
+
         AfterDestroy = 9
     }
 }
--- OWML.Common/Enums/MessageType.cs
+++ OWML.Common/Enums/MessageType.cs
@@ -3,11 +3,17 @@
     public enum MessageType
     {
         Message = 0,
+
         Error = 1,
+
         Warning = 2,
+
         Info = 3,
+
         Success = 4,
+
         Quit = 5,
+
         Fatal = 6
     }
 }
