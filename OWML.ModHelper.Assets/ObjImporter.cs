9a669137931d82ccd736c936e73d3f65f7ca6a61
Aleksander Waage <alek.ntnu@gmail.com>
2020-12-05 14:13:55 +0100

    cleaned up ObjImporter

--- OWML.ModHelper.Assets/ObjImporter.cs
+++ OWML.ModHelper.Assets/ObjImporter.cs
@@ -13,56 +13,43 @@ namespace OWML.ModHelper.Assets
 
     public class ObjImporter
     {
-
-        private struct MeshStruct
-        {
-            public Vector3[] vertices;
-            public Vector3[] normals;
-            public Vector2[] uv;
-            public Vector2[] uv1;
-            public Vector2[] uv2;
-            public int[] triangles;
-            public int[] faceVerts;
-            public int[] faceUVs;
-            public Vector3[] faceData;
-            public string name;
-            public string fileName;
-        }
-
-        // Use this for initialization
         public Mesh ImportFile(string filePath)
         {
             var newMesh = CreateMeshStruct(filePath);
             PopulateMeshStruct(ref newMesh);
 
-            var newVerts = new Vector3[newMesh.faceData.Length];
-            var newUVs = new Vector2[newMesh.faceData.Length];
-            var newNormals = new Vector3[newMesh.faceData.Length];
+            var newVertices = new Vector3[newMesh.FaceData.Length];
+            var newUVs = new Vector2[newMesh.FaceData.Length];
+            var newNormals = new Vector3[newMesh.FaceData.Length];
             var i = 0;
-            /* The following foreach loops through the facedata and assigns the appropriate vertex, uv, or normal
+            /* The following foreach loops through the face data and assigns the appropriate vertex, uv, or normal
              * for the appropriate Unity mesh array.
              */
-            foreach (var v in newMesh.faceData)
+            foreach (var v in newMesh.FaceData)
             {
-                newVerts[i] = newMesh.vertices[(int)v.x - 1];
+                newVertices[i] = newMesh.Vertices[(int)v.x - 1];
                 if (v.y >= 1)
-                    newUVs[i] = newMesh.uv[(int)v.y - 1];
+                {
+                    newUVs[i] = newMesh.Uv[(int)v.y - 1];
+                }
 
                 if (v.z >= 1)
-                    newNormals[i] = newMesh.normals[(int)v.z - 1];
+                {
+                    newNormals[i] = newMesh.Normals[(int)v.z - 1];
+                }
+
                 i++;
             }
 
             var mesh = new Mesh
             {
-                vertices = newVerts,
+                vertices = newVertices,
                 uv = newUVs,
                 normals = newNormals,
-                triangles = newMesh.triangles
+                triangles = newMesh.Triangles
             };
 
             mesh.RecalculateBounds();
-            //mesh.Optimize();
 
             return mesh;
         }
@@ -74,33 +61,30 @@ namespace OWML.ModHelper.Assets
             var vt = 0;
             var vn = 0;
             var face = 0;
-            var mesh = new MeshStruct
-            {
-                fileName = filename
-            };
+
             var stream = File.OpenText(filename);
             var entireText = stream.ReadToEnd();
             stream.Close();
+
             using (var reader = new StringReader(entireText))
             {
                 var currentText = reader.ReadLine();
                 char[] splitIdentifier = { ' ' };
-                string[] brokenString;
+
                 while (currentText != null)
                 {
-                    if (!currentText.StartsWith("f ") && !currentText.StartsWith("v ") && !currentText.StartsWith("vt ")
+                    if (!currentText.StartsWith("f ")
+                        && !currentText.StartsWith("v ")
+                        && !currentText.StartsWith("vt ")
                         && !currentText.StartsWith("vn "))
                     {
                         currentText = reader.ReadLine();
-                        if (currentText != null)
-                        {
-                            currentText = currentText.Replace("  ", " ");
-                        }
+                        currentText = currentText?.Replace("  ", " ");
                     }
                     else
                     {
-                        currentText = currentText.Trim();                           //Trim the current line
-                        brokenString = currentText.Split(splitIdentifier, 50);      //Split the line into an array, separating the original line by blank spaces
+                        currentText = currentText.Trim();
+                        var brokenString = currentText.Split(splitIdentifier, 50);
                         switch (brokenString[0])
                         {
                             case "v":
@@ -114,40 +98,41 @@ namespace OWML.ModHelper.Assets
                                 break;
                             case "f":
                                 face = face + brokenString.Length - 1;
-                                triangles = triangles + 3 * (brokenString.Length - 2); /*brokenString.Length is 3 or greater since a face must have at least
-                                                                                     3 vertices.  For each additional vertice, there is an additional
-                                                                                     triangle in the mesh (hence this formula).*/
+                                triangles += 3 * (brokenString.Length - 2);
+                                /* brokenString.Length is 3 or greater since a face must have at least
+                                 3 vertices.  For each additional vertex, there is an additional
+                                 triangle in the mesh (hence this formula).*/
                                 break;
                         }
                         currentText = reader.ReadLine();
-                        if (currentText != null)
-                        {
-                            currentText = currentText.Replace("  ", " ");
-                        }
+                        currentText = currentText?.Replace("  ", " ");
                     }
                 }
             }
-            mesh.triangles = new int[triangles];
-            mesh.vertices = new Vector3[vertices];
-            mesh.uv = new Vector2[vt];
-            mesh.normals = new Vector3[vn];
-            mesh.faceData = new Vector3[face];
-            return mesh;
+
+            return new MeshStruct
+            {
+                FileName = filename,
+                Triangles = new int[triangles],
+                Vertices = new Vector3[vertices],
+                Uv = new Vector2[vt],
+                Normals = new Vector3[vn],
+                FaceData = new Vector3[face]
+            };
         }
 
         private static void PopulateMeshStruct(ref MeshStruct mesh)
         {
-            var stream = File.OpenText(mesh.fileName);
+            var stream = File.OpenText(mesh.FileName);
             var entireText = stream.ReadToEnd();
             stream.Close();
+
             using (var reader = new StringReader(entireText))
             {
                 var currentText = reader.ReadLine();
 
                 char[] splitIdentifier = { ' ' };
                 char[] splitIdentifier2 = { '/' };
-                string[] brokenString;
-                string[] brokenBrokenString;
                 var f = 0;
                 var f2 = 0;
                 var v = 0;
@@ -155,69 +140,72 @@ namespace OWML.ModHelper.Assets
                 var vt = 0;
                 var vt1 = 0;
                 var vt2 = 0;
+
                 while (currentText != null)
                 {
-                    if (!currentText.StartsWith("f ") && !currentText.StartsWith("v ") && !currentText.StartsWith("vt ") &&
-                        !currentText.StartsWith("vn ") && !currentText.StartsWith("g ") && !currentText.StartsWith("usemtl ") &&
-                        !currentText.StartsWith("mtllib ") && !currentText.StartsWith("vt1 ") && !currentText.StartsWith("vt2 ") &&
-                        !currentText.StartsWith("vc ") && !currentText.StartsWith("usemap "))
+                    if (!currentText.StartsWith("f ") &&
+                        !currentText.StartsWith("v ") &&
+                        !currentText.StartsWith("vt ") &&
+                        !currentText.StartsWith("vn ") &&
+                        !currentText.StartsWith("g ") &&
+                        !currentText.StartsWith("usemtl ") &&
+                        !currentText.StartsWith("mtllib ") &&
+                        !currentText.StartsWith("vt1 ") &&
+                        !currentText.StartsWith("vt2 ") &&
+                        !currentText.StartsWith("vc ") &&
+                        !currentText.StartsWith("usemap "))
                     {
                         currentText = reader.ReadLine();
-                        if (currentText != null)
-                        {
-                            currentText = currentText.Replace("  ", " ");
-                        }
+                        currentText = currentText?.Replace("  ", " ");
                     }
                     else
                     {
                         currentText = currentText.Trim();
-                        brokenString = currentText.Split(splitIdentifier, 50);
+                        var brokenString = currentText.Split(splitIdentifier, 50);
                         switch (brokenString[0])
                         {
-                            case "g":
-                                break;
-                            case "usemtl":
-                                break;
-                            case "usemap":
-                                break;
-                            case "mtllib":
-                                break;
                             case "v":
-                                mesh.vertices[v] = new Vector3(System.Convert.ToSingle(brokenString[1]), System.Convert.ToSingle(brokenString[2]),
-                                                         System.Convert.ToSingle(brokenString[3]));
+                                mesh.Vertices[v] = new Vector3(
+                                    System.Convert.ToSingle(brokenString[1]), System.Convert.ToSingle(brokenString[2]),
+                                    System.Convert.ToSingle(brokenString[3]));
                                 v++;
                                 break;
                             case "vt":
-                                mesh.uv[vt] = new Vector2(System.Convert.ToSingle(brokenString[1]), System.Convert.ToSingle(brokenString[2]));
+                                mesh.Uv[vt] = new Vector2(
+                                    System.Convert.ToSingle(brokenString[1]),
+                                    System.Convert.ToSingle(brokenString[2]));
                                 vt++;
                                 break;
                             case "vt1":
-                                mesh.uv[vt1] = new Vector2(System.Convert.ToSingle(brokenString[1]), System.Convert.ToSingle(brokenString[2]));
+                                mesh.Uv[vt1] = new Vector2(
+                                    System.Convert.ToSingle(brokenString[1]),
+                                    System.Convert.ToSingle(brokenString[2]));
                                 vt1++;
                                 break;
                             case "vt2":
-                                mesh.uv[vt2] = new Vector2(System.Convert.ToSingle(brokenString[1]), System.Convert.ToSingle(brokenString[2]));
+                                mesh.Uv[vt2] = new Vector2(
+                                    System.Convert.ToSingle(brokenString[1]),
+                                    System.Convert.ToSingle(brokenString[2]));
                                 vt2++;
                                 break;
                             case "vn":
-                                mesh.normals[vn] = new Vector3(System.Convert.ToSingle(brokenString[1]), System.Convert.ToSingle(brokenString[2]),
-                                                        System.Convert.ToSingle(brokenString[3]));
+                                mesh.Normals[vn] = new Vector3(
+                                    System.Convert.ToSingle(brokenString[1]),
+                                    System.Convert.ToSingle(brokenString[2]),
+                                    System.Convert.ToSingle(brokenString[3]));
                                 vn++;
                                 break;
-                            case "vc":
-                                break;
                             case "f":
-
                                 var j = 1;
                                 var intArray = new List<int>();
                                 while (j < brokenString.Length && ("" + brokenString[j]).Length > 0)
                                 {
                                     var temp = new Vector3();
-                                    brokenBrokenString = brokenString[j].Split(splitIdentifier2, 3);    //Separate the face into individual components (vert, uv, normal)
+                                    var brokenBrokenString = brokenString[j].Split(splitIdentifier2, 3);
                                     temp.x = System.Convert.ToInt32(brokenBrokenString[0]);
-                                    if (brokenBrokenString.Length > 1)                                  //Some .obj files skip UV and normal
+                                    if (brokenBrokenString.Length > 1) // Some .obj files skip UV and normal
                                     {
-                                        if (brokenBrokenString[1] != "")                                    //Some .obj files skip the uv and not the normal
+                                        if (brokenBrokenString[1] != "") // Some .obj files skip the uv and not the normal
                                         {
                                             temp.y = System.Convert.ToInt32(brokenBrokenString[1]);
                                         }
@@ -225,18 +213,18 @@ namespace OWML.ModHelper.Assets
                                     }
                                     j++;
 
-                                    mesh.faceData[f2] = temp;
+                                    mesh.FaceData[f2] = temp;
                                     intArray.Add(f2);
                                     f2++;
                                 }
                                 j = 1;
-                                while (j + 2 < brokenString.Length)     //Create triangles out of the face data.  There will generally be more than 1 triangle per face.
+                                while (j + 2 < brokenString.Length) // Create triangles out of the face data. There will generally be more than 1 triangle per face.
                                 {
-                                    mesh.triangles[f] = intArray[0];
+                                    mesh.Triangles[f] = intArray[0];
                                     f++;
-                                    mesh.triangles[f] = intArray[j];
+                                    mesh.Triangles[f] = intArray[j];
                                     f++;
-                                    mesh.triangles[f] = intArray[j + 1];
+                                    mesh.Triangles[f] = intArray[j + 1];
                                     f++;
 
                                     j++;
@@ -244,13 +232,25 @@ namespace OWML.ModHelper.Assets
                                 break;
                         }
                         currentText = reader.ReadLine();
-                        if (currentText != null)
-                        {
-                            currentText = currentText.Replace("  ", " ");       //Some .obj files insert double spaces, this removes them.
-                        }
+                        currentText = currentText?.Replace("  ", " "); // Some .obj files insert double spaces, this removes them.
                     }
                 }
             }
+        }
+
+        private struct MeshStruct
+        {
+            public Vector3[] Vertices { get; set; }
+
+            public Vector3[] Normals { get; set; }
+
+            public Vector2[] Uv { get; set; }
+
+            public int[] Triangles { get; set; }
+
+            public Vector3[] FaceData { get; set; }
+
+            public string FileName { get; set; }
         }
     }
 }
