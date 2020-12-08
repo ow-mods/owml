1fcceace06a081cb0723bfeb444f7a3dae024c8e
Aleksander Waage <alek.ntnu@gmail.com>
2020-12-08 06:03:26 +0100

    fixed DI issue

--- src/OWML.Utils/Container.cs
+++ src/OWML.Utils/Container.cs
@@ -8,19 +8,19 @@ namespace OWML.Utils
 
         public Container Add<TInterface>(TInterface instance)
         {
-            _container.RegisterInstance(instance);
+            _container.RegisterInstance(instance, new ContainerControlledLifetimeManager());
             return this;
         }
 
         public Container Add<TInterface, TImplementation>() where TImplementation : TInterface
         {
-            _container.RegisterType<TInterface, TImplementation>();
+            _container.RegisterType<TInterface, TImplementation>(new ContainerControlledLifetimeManager());
             return this;
         }
 
         public Container Add<TImplementation>()
         {
-            _container.RegisterType<TImplementation>();
+            _container.RegisterType<TImplementation>(new ContainerControlledLifetimeManager());
             return this;
         }
 
