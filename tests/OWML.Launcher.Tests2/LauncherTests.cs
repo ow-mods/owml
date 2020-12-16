27656c033ffb38c720ef3b16991e86a1979d277f
Aleksander Waage <alek.ntnu@gmail.com>
2020-12-16 21:10:37 +0100

    upgraded some more tests

---
+++ tests/OWML.Launcher.Tests2/LauncherTests.cs
@@ -0,0 +1,51 @@
+﻿using Moq;
+using OWML.Common;
+using OWML.Tests.Setup;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OWML.Launcher.Tests
+{
+	public class LauncherTests : OWMLTests
+	{
+		public LauncherTests(ITestOutputHelper outputHelper)
+			: base(outputHelper)
+		{
+		}
+
+		[Fact]
+		public void Run_StartsGame()
+		{
+			var processHelper = new Mock<IProcessHelper>();
+
+			var container = Program.CreateContainer(new[] { "-consolePort", "1337" });
+			container.Add(processHelper.Object);
+			container.Add(Console.Object);
+			container.Add(Logger.Object);
+
+			var app = container.Resolve<App>();
+			app.Run();
+
+			processHelper.Verify(s => s.Start(Config.ExePath, new string[] { }), Times.Once);
+		}
+
+		[Fact]
+		public void Run_OldVersion_StartsGame()
+		{
+			Config.GamePath = "C:/Program Files (x86)/Outer Wilds";
+
+			var processHelper = new Mock<IProcessHelper>();
+
+			var container = Program.CreateContainer(new[] { "-consolePort", "1337" });
+			container.Add(processHelper.Object);
+			container.Add(Console.Object);
+			container.Add(Logger.Object);
+			container.Add(Config);
+
+			var app = container.Resolve<App>();
+			app.Run();
+
+			processHelper.Verify(s => s.Start(Config.ExePath, new string[] { }), Times.Once);
+		}
+	}
+}
---
+++ tests/OWML.ModLoader.Tests2/ModLoaderTests.cs
@@ -0,0 +1,40 @@
+﻿using OWML.Tests.Setup;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OWML.ModLoader.Tests
+{
+	public class ModLoaderTests : OWMLTests
+	{
+		public ModLoaderTests(ITestOutputHelper outputHelper)
+			: base(outputHelper)
+		{
+		}
+
+		[Fact]
+		public void ModLoader_LoadMods()
+		{
+			var container = ModLoader.CreateContainer(AppHelper.Object, GOHelper.Object);
+			container.Add(Console.Object);
+			container.Add(Logger.Object);
+			container.Add(Config);
+
+			var owo = container.Resolve<Owo>();
+			owo.LoadMods();
+		}
+
+		[Fact]
+		public void ModLoader_OldVersion_LoadMods()
+		{
+			Config.GamePath = "C:/Program Files (x86)/Outer Wilds";
+
+			var container = ModLoader.CreateContainer(AppHelper.Object, GOHelper.Object);
+			container.Add(Console.Object);
+			container.Add(Logger.Object);
+			container.Add(Config);
+
+			var owo = container.Resolve<Owo>();
+			owo.LoadMods();
+		}
+	}
+}
