77bcf3bf33a0acc9494597d7cb0b44a59de1209d
Aleksander Waage <alek.ntnu@gmail.com>
2020-12-18 21:20:52 +0100

    cleanup

--- src/OWML.Logging/ModLogger.cs
+++ src/OWML.Logging/ModLogger.cs
@@ -7,20 +7,13 @@ namespace OWML.Logging
 {
 	public class ModLogger : IModLogger
 	{
-		private static IOwmlConfig _config;
 		private readonly IModManifest _manifest;
 		private static string _logFileName;
 
 		public ModLogger(IOwmlConfig config, IModManifest manifest)
 		{
-			if (_config == null)
-			{
-				_config = config;
-			}
 			_manifest = manifest;
-
-			var startTime = DateTime.Now.ToString("dd-MM-yyyy-HH.mm.ss");
-			_logFileName = $"{config.LogsPath}/OWML.Log.{startTime}.txt";
+			_logFileName = $"{config.LogsPath}/OWML.Log.{DateTime.Now:dd-MM-yyyy-HH.mm.ss}.txt";
 
 			if (!Directory.Exists(config.LogsPath))
 			{
