6cb6942def637c49ab2a34d317fa91b8d47c29fd
Aleksander Waage <alek.ntnu@gmail.com>
2021-09-29 20:12:57 +0200

    json fixes

--- src/OWML.ModLoader/ModLoader.cs
+++ src/OWML.ModLoader/ModLoader.cs
@@ -11,59 +11,65 @@ using UnityEngine;
 
 namespace OWML.ModLoader
 {
-	public class ModLoader
-	{
-		public static void LoadMods()
-		{
-			var appHelper = new ApplicationHelper();
-			var goHelper = new GameObjectHelper();
-			var container = CreateContainer(appHelper, goHelper);
+    public class ModLoader
+    {
+        public static void LoadMods()
+        {
+            var appHelper = new ApplicationHelper();
+            var goHelper = new GameObjectHelper();
+            var container = CreateContainer(appHelper, goHelper);
 
-			var owo = container.Resolve<Owo>();
-			owo.LoadMods();
-		}
+            var owo = container.Resolve<Owo>();
+            owo.LoadMods();
+        }
 
-		public static Container CreateContainer(IApplicationHelper appHelper, IGameObjectHelper goHelper)
-		{
-			var owmlConfig = JsonHelper.LoadJsonObject<OwmlConfig>($"{appHelper.DataPath}/Managed/{Constants.OwmlConfigFileName}");
-			var owmlManifest = JsonHelper.LoadJsonObject<ModManifest>($"{appHelper.DataPath}/Managed/{Constants.OwmlManifestFileName}");
+        public static Container CreateContainer(IApplicationHelper appHelper, IGameObjectHelper goHelper)
+        {
+            var configPath = $"{appHelper.DataPath}/Managed/{Constants.OwmlConfigFileName}";
+            var owmlConfig = JsonHelper.LoadJsonObject<OwmlConfig>(configPath);
+            if (owmlConfig == null)
+            {
+                throw new UnityException($"Can't load OWML config: {configPath}");
+            }
 
-			if (owmlConfig == null || owmlManifest == null)
-			{
-				throw new UnityException("Can't load OWML config or manifest.");
-			}
+            var manifestPath = $"{appHelper.DataPath}/Managed/{Constants.OwmlManifestFileName}";
+            var owmlManifest = JsonHelper.LoadJsonObject<ModManifest>(manifestPath);
+            if (owmlManifest == null)
+            {
+                throw new UnityException($"Can't load OWML manifest: {manifestPath}");
+            }
 
-			var unityEvents = goHelper.CreateAndAdd<IModUnityEvents, ModUnityEvents>();
+            var unityEvents = goHelper.CreateAndAdd<IModUnityEvents, ModUnityEvents>();
 
-			return new Container()
-				.Add(appHelper)
-				.Add(goHelper)
-				.Add(unityEvents)
-				.Add<IOwmlConfig>(owmlConfig)
-				.Add<IModManifest>(owmlManifest)
-				.Add<IModLogger, ModLogger>()
-				.Add<IModSocket, ModSocket>()
-				.Add<IUnityLogger, UnityLogger>()
-				.Add<IModConsole, ModSocketOutput>()
-				.Add<IModSorter, ModSorter>()
-				.Add<IModFinder, ModFinder>()
-				.Add<IHarmonyHelper, HarmonyHelper>()
-				.Add<IModPlayerEvents, ModPlayerEvents>()
-				.Add<IModSceneEvents, ModSceneEvents>()
-				.Add<IModEvents, ModEvents>()
-				.Add<IModStorage, ModStorage>()
-				.Add<IModConfigMenuBase, OwmlConfigMenu>()
-				.Add<IModTabbedMenu, ModOptionsMenu>()
-				.Add<IModMainMenu, ModMainMenu>()
-				.Add<IModPauseMenu, ModPauseMenu>()
-				.Add<IModsMenu, ModsMenu>()
-				.Add<IModInputMenu, ModInputMenu>()
-				.Add<IModMessagePopup, ModMessagePopup>()
-				.Add<IModPopupManager, ModPopupManager>()
-				.Add<IModMenus, ModMenus>()
-				.Add<IObjImporter, ObjImporter>()
-				.Add<IProcessHelper, ProcessHelper>()
-				.Add<Owo>();
-		}
-	}
+            return new Container()
+                .Add(appHelper)
+                .Add(goHelper)
+                .Add(unityEvents)
+                .Add<IOwmlConfig>(owmlConfig)
+                .Add<IModManifest>(owmlManifest)
+                .Add<IModLogger, ModLogger>()
+                .Add<IModSocket, ModSocket>()
+                .Add<IUnityLogger, UnityLogger>()
+                .Add<IModConsole, ModSocketOutput>()
+                .Add<IModSorter, ModSorter>()
+                .Add<IModFinder, ModFinder>()
+                .Add<IHarmonyHelper, HarmonyHelper>()
+                .Add<IModPlayerEvents, ModPlayerEvents>()
+                .Add<IModSceneEvents, ModSceneEvents>()
+                .Add<IModEvents, ModEvents>()
+                .Add<IModStorage, ModStorage>()
+                .Add<IModConfigMenuBase, OwmlConfigMenu>()
+                .Add<IModTabbedMenu, ModOptionsMenu>()
+                .Add<IModMainMenu, ModMainMenu>()
+                .Add<IModPauseMenu, ModPauseMenu>()
+                .Add<IModsMenu, ModsMenu>()
+                .Add<IModInputMenu, ModInputMenu>()
+                .Add<IModMessagePopup, ModMessagePopup>()
+                .Add<IModPopupManager, ModPopupManager>()
+                .Add<IModMenus, ModMenus>()
+                .Add<IObjImporter, ObjImporter>()
+                .Add<IProcessHelper, ProcessHelper>()
+                .Add<Owo>();
+        }
+    }
 }
--- src/OWML.Utils/JsonHelper.cs
+++ src/OWML.Utils/JsonHelper.cs
@@ -4,30 +4,30 @@ using Newtonsoft.Json;
 
 namespace OWML.Utils
 {
-	public static class JsonHelper
-	{
-		public static T LoadJsonObject<T>(string path)
-		{
-			if (!File.Exists(path))
-			{
-				return default;
-			}
+    public static class JsonHelper
+    {
+        public static T LoadJsonObject<T>(string path)
+        {
+            if (!File.Exists(path))
+            {
+                return default;
+            }
 
-			var json = File.ReadAllText(path)
-				.Replace("\\\\", "/")
-				.Replace("\\", "/");
+            var json = File.ReadAllText(path)
+                .Replace("\\\\", "/")
+                .Replace("\\", "/");
 
-			try
-			{
-				return JsonConvert.DeserializeObject<T>(json);
-			}
-			catch (Exception)
-			{
-				return default;
-			}
-		}
+            //try
+            //{
+            return JsonConvert.DeserializeObject<T>(json);
+            //}
+            //catch (Exception)
+            //{
+            //	return default;
+            //}
+        }
 
-		public static void SaveJsonObject<T>(string path, T obj) =>
-			File.WriteAllText(path, JsonConvert.SerializeObject(obj, Formatting.Indented));
-	}
+        public static void SaveJsonObject<T>(string path, T obj) =>
+            File.WriteAllText(path, JsonConvert.SerializeObject(obj, Formatting.Indented));
+    }
 }
