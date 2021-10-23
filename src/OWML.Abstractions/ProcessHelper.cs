70e4df43135ef7574afa94fe75598a63372c366f
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2021-10-23 16:24:47 +0100

    add stuff

--- src/OWML.Abstractions/ProcessHelper.cs
+++ src/OWML.Abstractions/ProcessHelper.cs
@@ -5,8 +5,16 @@ namespace OWML.Abstractions
 {
 	public class ProcessHelper : IProcessHelper
 	{
-		public void Start(string path, string[] args) =>
+		public void Start(string path, string[] args = null)
+		{
+			if (args is null)
+			{
+				Process.Start(path);
+				return;
+			}
+
 			Process.Start(path, string.Join(" ", args));
+		}
 
 		public void KillCurrentProcess() =>
 			Process.GetCurrentProcess().Kill();
--- src/OWML.Launcher/App.cs
+++ src/OWML.Launcher/App.cs
@@ -162,13 +162,26 @@ namespace OWML.Launcher
 
 		private void StartGame()
 		{
-			_writer.WriteLine("Starting game...");
-
 			_argumentHelper.RemoveArgument("consolePort");
 
 			try
 			{
-				_processHelper.Start(_owmlConfig.ExePath, _argumentHelper.Arguments);
+				if (_owmlConfig.GamePath.Contains("Epic"))
+				{
+					_writer.WriteLine("Starting game via Epic Launcher...");
+					_processHelper.Start("\"com.epicgames.launcher://apps/starfish%3A601d0668cef146bd8eef75d43c6bbb0b%3AStarfish?action=launch&silent=true\"");
+				}
+				else if (_owmlConfig.GamePath.Contains("Steam"))
+				{
+					_writer.WriteLine("Starting game via Steam...");
+					_processHelper.Start("steam://rungameid/753640");
+				}
+				else
+				{
+					_writer.WriteLine("Starting game via exe...");
+					_processHelper.Start(_owmlConfig.ExePath, _argumentHelper.Arguments);
+				}
+				
 			}
 			catch (Exception ex)
 			{
