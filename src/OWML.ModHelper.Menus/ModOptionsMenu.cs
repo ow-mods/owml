8589c5666745939b445d9b0813184f0d9e5d2578
Aleksander Waage <alek.ntnu@gmail.com>
2021-10-29 08:12:21 +0200

    foreach

--- src/OWML.ModHelper.Menus/ModOptionsMenu.cs
+++ src/OWML.ModHelper.Menus/ModOptionsMenu.cs
@@ -88,9 +88,9 @@ namespace OWML.ModHelper.Menus
 		private void UpdateTabNavigation()
 		{
 			Selectable previous = null, first = null;
-			for (var i = 0; i < _tabMenus.Count; i++)
+			foreach (var tabMenu in _tabMenus)
 			{
-				var current = _tabMenus[i].TabButton.GetSelectable();
+				var current = tabMenu.TabButton.GetSelectable();
 				if (!(current?.gameObject.activeSelf ?? false))
 				{
 					continue;
