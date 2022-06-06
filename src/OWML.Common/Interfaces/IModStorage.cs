cf2f92a849438273bf121c85eb952fe58234f824
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2022-06-06 23:21:27 +0100

    add json optional settings into ModStorage.Load

--- src/OWML.Common/Interfaces/IModStorage.cs
+++ src/OWML.Common/Interfaces/IModStorage.cs
@@ -1,8 +1,10 @@
-﻿namespace OWML.Common
+﻿using Newtonsoft.Json;
+
+namespace OWML.Common
 {
 	public interface IModStorage
 	{
-		T Load<T>(string filename);
+		T Load<T>(string filename, bool fixBackslashes = true, JsonSerializerSettings settings = null);
 
 		void Save<T>(T obj, string filename);
 	}
