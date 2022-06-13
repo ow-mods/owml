f685acf1998a0c5f326d24b5fc3895a42f3193ed
Nick <nicholasconnors@hotmail.ca>
2022-06-13 08:16:25 -0400

    Fixed #452 (exceptions in mod settings break options for other mods)

--- src/OWML.ModHelper.Menus/ModConfigMenu.cs
+++ src/OWML.ModHelper.Menus/ModConfigMenu.cs
@@ -1,10 +1,11 @@
 ﻿using OWML.Common;
-using System.Linq;
 using OWML.Common.Menus;
+using System;
+using System.Linq;
 
 namespace OWML.ModHelper.Menus
 {
-	public class ModConfigMenu : ModConfigMenuBase, IModConfigMenu
+    public class ModConfigMenu : ModConfigMenuBase, IModConfigMenu
 	{
 		private const string EnabledTitle = "Enabled";
 
@@ -53,7 +54,17 @@ namespace OWML.ModHelper.Menus
 				}
 			}
 			ModData.Storage.Save(ModData.Config, Constants.ModConfigFileName);
-			Mod?.Configure(ModData.Config);
+
+			// Fixes "If one mod throws an exception in Configure all mod settings break" (#452)
+			try
+			{
+				Mod?.Configure(ModData.Config);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Exception thrown when changing mod settings: {e.Message}, {e.StackTrace}", MessageType.Error);
+			}
+
 			Close();
 		}
 
