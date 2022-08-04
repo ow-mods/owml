5027645deed6451f9899554ac88e9145ee97ecc2
Noah Pilarski <mehmehmeh313@gmail.com>
2022-08-04 01:04:40 -0400

    Stop tabbed menu from logging all the time

--- src/OWML.ModLoader/Owo.cs
+++ src/OWML.ModLoader/Owo.cs
@@ -73,6 +73,7 @@ namespace OWML.ModLoader
 
 			_goHelper.CreateAndAdd<OwmlBehaviour>();
 			_unityLogger.Start();
+			_harmonyHelper.AddPrefix<TabbedMenu>(nameof(TabbedMenu.OnUpdateInputDevice), typeof(Owo), nameof(TabbedMenu_OnUpdateInputDevice));
 			var mods = _modFinder.GetMods();
 
 			var changedSettings = mods.Where(mod => mod.FixConfigs()).Select(mod => mod.Manifest.Name).ToArray();
@@ -215,6 +216,15 @@ namespace OWML.ModLoader
 				_console.WriteLine($"Error while adding/initializing {helper.Manifest.UniqueName}: {ex}", MessageType.Error);
 				return null;
 			}
+		}
+
+		private static bool TabbedMenu_OnUpdateInputDevice(TabbedMenu __instance)
+		{
+			if ((object)__instance == null) return false;
+			if (__instance == null) return false;
+			if (__instance.gameObject == null) return false;
+			if (Utils.TypeExtensions.GetValue<UnityEngine.UI.Image>(__instance, "_tabLeftButtonImg") == null || Utils.TypeExtensions.GetValue<UnityEngine.UI.Image>(__instance, "_tabRightButtonImg") == null) return false;
+			return true;
 		}
 	}
 }
