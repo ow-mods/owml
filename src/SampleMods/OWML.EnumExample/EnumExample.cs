a2ce20a9d968929cd1740f5178f482f67030a457
Noah Pilarski <mehmehmeh313@gmail.com>
2022-08-19 22:36:45 -0400

    Add more examples

--- src/SampleMods/OWML.EnumExample/EnumExample.cs
+++ src/SampleMods/OWML.EnumExample/EnumExample.cs
@@ -1,6 +1,8 @@
 ﻿using OWML.Common;
 using OWML.ModHelper;
 using OWML.Utils;
+using System;
+using System.Linq;
 
 namespace OWML.EnumExample
 {
@@ -17,6 +19,15 @@ namespace OWML.EnumExample
 		{
 			ModHelper.Console.WriteLine($"Checking name of enums", MessageType.Info);
 
+			var createdEnum = EnumUtils.Create<Sector.Name>("Test");
+
+
+			if (CustomEnums.Magic.ToString() == nameof(CustomEnums.Magic))
+				ModHelper.Console.WriteLine("Magic enum has correct name", MessageType.Success);
+			else
+				ModHelper.Console.WriteLine("Magic enum does not have correct name", MessageType.Error);
+
+
 			if (CustomEnums.Illuminati.ToString() == nameof(CustomEnums.Illuminati))
 			{
 				ModHelper.Console.WriteLine("Illuminati enum has correct name", MessageType.Success);
@@ -28,12 +39,27 @@ namespace OWML.EnumExample
 			else
 				ModHelper.Console.WriteLine("Illuminati enum does not have correct name", MessageType.Error);
 
-			if (CustomEnums.Magic.ToString() == nameof(CustomEnums.Magic))
-				ModHelper.Console.WriteLine("Magic enum has correct name", MessageType.Success);
-			else
-				ModHelper.Console.WriteLine("Magic enum does not have correct name", MessageType.Error);
 
-			var createdEnum = EnumUtils.Create<Sector.Name>("Test");
+			if (Enum.IsDefined(typeof(Sector.Name), 666))
+				ModHelper.Console.WriteLine("Illuminati enum is defined", MessageType.Success);
+			else
+				ModHelper.Console.WriteLine("Illuminati enum is not defined", MessageType.Error);
+
+
+			var names = Enum.GetNames(typeof(Sector.Name));
+			if (names.Contains("Illuminati") && names.Contains("Test"))
+				ModHelper.Console.WriteLine("Sector.Name contains Illuminati and Test", MessageType.Success);
+			else
+				ModHelper.Console.WriteLine("Sector.Name does not contain Illuminati and Test", MessageType.Error);
+
+
+			var values = Enum.GetValues(typeof(Sector.Name)).Cast<int>();
+			if (values.Contains(666) && values.Contains((int)createdEnum))
+				ModHelper.Console.WriteLine("Sector.Name contains values 666 and " + ((int)createdEnum), MessageType.Success);
+			else
+				ModHelper.Console.WriteLine("Sector.Name does not contain values 666 and " + ((int)createdEnum), MessageType.Error);
+
+
 			if (createdEnum.ToString() == "Test")
 			{
 				ModHelper.Console.WriteLine("Test enum has correct name", MessageType.Success);
