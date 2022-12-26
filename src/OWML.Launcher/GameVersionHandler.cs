bab06a74acdace95c5ab1641934e4a7cf742b7fb
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2022-12-26 15:20:28 +0000

    better dialogue for gameversionhandler

--- src/OWML.Launcher/GameVersionHandler.cs
+++ src/OWML.Launcher/GameVersionHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Forms;
 using OWML.Common;
 
 namespace OWML.Launcher
@@ -8,12 +9,14 @@ namespace OWML.Launcher
 		private readonly IGameVersionReader _versionReader;
 		private readonly IModConsole _writer;
 		private readonly IModManifest _owmlManifest;
+		private readonly IProcessHelper _processHelper;
 
-		public GameVersionHandler(IGameVersionReader versionReader, IModConsole writer, IModManifest owmlManifest)
+		public GameVersionHandler(IGameVersionReader versionReader, IModConsole writer, IModManifest owmlManifest, IProcessHelper processHelper)
 		{
 			_versionReader = versionReader;
 			_writer = writer;
 			_owmlManifest = owmlManifest;
+			_processHelper = processHelper;
 		}
 
 		public void CompareVersions()
@@ -25,21 +28,37 @@ namespace OWML.Launcher
 			var minVersion = new Version(_owmlManifest.MinGameVersion);
 			var maxVersion = new Version(_owmlManifest.MaxGameVersion);
 
+			var showPopup = false;
+			var reason = "";
+
 			if (!isValidFormat)
 			{
-				_writer.WriteLine("Warning - invalid game version format", MessageType.Warning);
+				showPopup = true;
+				reason = "Invalid game version format.";
+				_writer.WriteLine("Warning - Invalid game version format", MessageType.Warning);
 			}
 			else if (gameVersion > maxVersion)
 			{
-				_writer.WriteLine("Potentially unsupported game version found, continue at your own risk", MessageType.Warning);
+				showPopup = true;
+				reason = $"New Outer Wilds version. This version of OWML was built for {maxVersion}.";
+				_writer.WriteLine($"New Outer Wilds version. This version of OWML was built for {maxVersion}.", MessageType.Warning);
 			}
 			else if (gameVersion < minVersion)
 			{
-				_writer.WriteLine("Unsupported game version found, custom menus won't work.", MessageType.Warning);
+				showPopup = true;
+				reason = $"Outdated Outer Wilds version. The oldest compatible version is {minVersion}.";
+				_writer.WriteLine($"Outdated Outer Wilds version. The oldest compatible version is {minVersion}.", MessageType.Warning);
 			}
-			else
+
+			if (showPopup)
 			{
-				_writer.WriteLine("Game version is supported", MessageType.Success);
+				var result = MessageBox.Show($"OWML could not verify if it will work on this version of Outer Wilds ({gameVersionString}).\n\n" +
+					$"Reason: {reason}\n\n" +
+					$"Do you want to continue?", "OWML.Launcher.GameVersionHandler", MessageBoxButtons.YesNo);
+				if (result == DialogResult.No)
+				{
+					_processHelper.KillCurrentProcess();
+				}
 			}
 		}
 	}
