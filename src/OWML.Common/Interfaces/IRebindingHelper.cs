2d0b7c80f589c42ca62aff0c88d58093b5fe12d1
_nebula <41904486+misternebula@users.noreply.github.com>
2023-05-22 00:34:52 +0100

    create rebinding helper

---
+++ src/OWML.Common/Interfaces/IRebindingHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+namespace OWML.Common
+{
+	public interface IRebindingHelper
+	{
+		public List<RebindableID> Rebindables { get; }
+
+		public InputConsts.InputCommandType RegisterRebindableAxis(string name, string primary);
+
+		public InputAction RegisterCustomAction(string name);
+
+		public void AddBinding(InputAction action, string control);
+	}
+}
---
+++ src/OWML.ModHelper.Input/RebindingHelper.cs
@@ -0,0 +1,51 @@
+﻿using OWML.Common;
+using OWML.Utils;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+namespace OWML.ModHelper.Input
+{
+	public class RebindingHelper : IRebindingHelper
+	{
+		public static InputActionMap CustomActionMap = new("OWMLCustomActionMap");
+
+		public List<RebindableID> Rebindables { get; } = new List<RebindableID>();
+
+		private readonly IModConsole _console;
+
+		public RebindingHelper(IModConsole console)
+		{
+			_console = console;
+		}
+
+		public IInputCommands GetCommand(InputConsts.InputCommandType commandType) => InputLibrary.GetInputCommand(commandType);
+
+		// must be called in Start()
+		public InputConsts.InputCommandType RegisterRebindableAxis(string name, string primary)
+		{
+			_console.WriteLine($"RegisterRebindableAxis name:{name} primary:{primary}");
+
+			var commandType = EnumUtils.Create<InputConsts.InputCommandType>(name);
+			var rebindableId = EnumUtils.Create<RebindableID>(primary);
+
+			var inputCommandData = InputCommandDefinitions.InputCommandData.CreateCommandData(CommandDataType.Axis, commandType);
+			inputCommandData.TrySetAsAxis(rebindableId, primary);
+			InputCommandDefinitions.AddInputCommandData(inputCommandData);
+
+			Rebindables.Add(rebindableId);
+
+			return commandType;
+		}
+
+		public InputAction RegisterCustomAction(string name)
+			=> CustomActionMap.AddAction(
+				name,
+				InputActionType.Button,
+				interactions: "OWInput",
+				processors: "OWAxis",
+				expectedControlLayout: "Button");
+
+		public void AddBinding(InputAction action, string control)
+			=> CustomActionMap.AddBinding(control, action, groups: "KeyboardMouse");
+	}
+}
