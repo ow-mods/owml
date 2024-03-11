168b4d8ccfa5776bb1298e87b4b4abfbf7bc98db
_nebula <41904486+misternebula@users.noreply.github.com>
2024-03-11 20:33:10 +0000

    add comments and documentation

--- src/OWML.Common/Constants.cs
+++ src/OWML.Common/Constants.cs
@@ -1,7 +1,10 @@
 ﻿using System.Runtime.CompilerServices;
 
+// make everything in OWML.Common visible to these namespaces
+// this is in this file just because it's the first one that comes up in VS :P
 [assembly: InternalsVisibleTo("OWML.ModHelper.Menus")]
 [assembly: InternalsVisibleTo("OWML.ModLoader")]
+
 namespace OWML.Common
 {
 	public class Constants
