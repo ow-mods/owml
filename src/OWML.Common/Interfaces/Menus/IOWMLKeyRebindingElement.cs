3badc877c0041dd9560e93ee5769a725f744003f
_nebula <41904486+misternebula@users.noreply.github.com>
2026-04-11 22:01:33 +0100

    Remove IOWMLKeyRebindingElement

--- src/OWML.Common/Interfaces/Menus/IOWMLKeyRebindingElement.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OWML.Common.Interfaces.Menus
-{
-	public interface IOWMLKeyRebindingElement
-	{
-		void Initialize(SettingsMenuModel settingsMenuModel);
-		void UpdateDisplay(bool forceRefresh = false);
-		RebindableID GetRebindableID();
-	}
-}
