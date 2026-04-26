8556068ad002fdc5670e53eff80d352c78a516b8
_nebula <41904486+misternebula@users.noreply.github.com>
2026-04-26 01:59:38 +0100

    fix for wine/crossover manifest issues

---
+++ src/OWML.Common/WineChecker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace OWML.Common
+{
+	public static class WineChecker
+	{
+		[DllImport("ntdll.dll", CallingConvention = CallingConvention.Cdecl)]
+		private static extern IntPtr wine_get_version();
+
+		public static bool IsUsingWine()
+		{
+			try
+			{
+				return wine_get_version() != IntPtr.Zero;
+			}
+			catch (EntryPointNotFoundException)
+			{
+				return false;
+			}
+		}
+	}
+}
